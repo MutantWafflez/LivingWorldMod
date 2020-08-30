f9a6907b7dc4fe921cf3fb7d9671254a1f2685b8
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2020-08-29 22:12:28 -0700

    Harpy Reputation Loss

--- LWMWorld.cs
+++ LWMWorld.cs
@@ -1,4 +1,5 @@
 ﻿using LivingWorldMod.NPCs.Villagers;
+using Microsoft.Xna.Framework;
 using System;
 using Terraria;
 using Terraria.ModLoader;
@@ -9,6 +10,38 @@ namespace LivingWorldMod
     public class LWMWorld : ModWorld
     {
         public static int[] villageReputation = new int[(int)VillagerType.VillagerTypeCount];
+
+        /// <summary>
+        /// Changes the inputted VillagerType's reputation by amount.
+        /// </summary>
+        /// <param name="villagerType">Enum of VillagerType</param>
+        /// <param name="amount">Amount by which the reputation is changed</param>
+        public static void ModifyReputation(VillagerType villagerType, int amount)
+        {
+            if (villagerType >= 0 && villagerType < VillagerType.VillagerTypeCount)
+            {
+                villageReputation[(int)villagerType] += amount;
+            }
+        }
+
+        /// <summary>
+        /// Changes the inputted VillagerType's reputation by amount, and creates a combat text by the changed amount at combatTextPosition.
+        /// </summary>
+        /// <param name="villagerType">Enum of VillagerType</param>
+        /// <param name="amount">Amount by which the reputation is changed</param>
+        /// <param name="combatTextPosition">Location of the combat text created to signify the changed reputation amount</param>
+        public static void ModifyReputation(VillagerType villagerType, int amount, Rectangle combatTextPosition)
+        {
+            if (villagerType >= 0 && villagerType < VillagerType.VillagerTypeCount)
+            {
+                villageReputation[(int)villagerType] += amount;
+
+                Color combatTextColor = new Color(255, 0, 0);
+                if (amount > 0)
+                    combatTextColor = new Color(0, 255, 0);
+                CombatText.NewText(combatTextPosition, combatTextColor, (amount > 0 ? "+" : "") + amount + " Reputation", true);
+            }
+        }
 
         public override TagCompound Save()
         {
---
+++ NPCs/ReputationGlobalNPC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using LivingWorldMod.NPCs.Villagers;
+using Microsoft.Xna.Framework;
+
+namespace LivingWorldMod.NPCs
+{
+    public class ReputationGlobalNPC : GlobalNPC
+    {
+
+
+        public override bool SpecialNPCLoot(NPC npc)
+        {
+            switch (npc.type)
+            {
+                case NPCID.Harpy:
+                    LWMWorld.ModifyReputation(VillagerType.SkyVillager, -1, new Rectangle((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height));
+                    break;
+                case NPCID.WyvernHead:
+                    LWMWorld.ModifyReputation(VillagerType.SkyVillager, -5, new Rectangle((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height));
+                    break;
+            }
+            return base.SpecialNPCLoot(npc);
+        }
+    }
+}
