19631086cec313c088016b831792ab5e5fa0c1c5
xKirtle <xkirtle26@gmail.com>
2020-08-31 03:36:11 +0100

    Added Spider Caves mapping
    Also added progress for the WorldGenTask progress bar

--- Utils/LWMUtils.cs
+++ Utils/LWMUtils.cs
@@ -1,4 +1,5 @@
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.Utilities;
 
 namespace LivingWorldMod.Utils
@@ -12,5 +13,9 @@ namespace LivingWorldMod.Utils
                 weightedRandom.Add(curType, weight);
             }
         }
+
+        //----------Extension Methods----------
+        public static Tile ToTile(this TileNode tn) => Framing.GetTileSafely(tn.position);
+        public static Point16 Add(this Point16 point, int p1x, int p1y) => point + new Point16(p1x, p1y);
     }
 }
