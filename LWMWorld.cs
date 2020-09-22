485e753679cb4b492195e1f8295cab73800180d6
xKirtle <xkirtle26@gmail.com>
2020-09-22 03:28:23 +0100

    Refined WorldGen and made Spider Sacs regenerate over time in the world

--- LWMWorld.cs
+++ LWMWorld.cs
@@ -9,16 +9,18 @@ using Terraria.GameContent.Generation;
 using static Terraria.ModLoader.ModContent;
 using LivingWorldMod.Tiles.WorldGen;
 using LivingWorldMod.NPCs.Villagers;
-using LivingWorldMod.Utils;
 
 namespace LivingWorldMod
 {
     public class LWMWorld : ModWorld
     {
         public static int[] villageReputation = new int[(int)VillagerType.VillagerTypeCount];
-        public bool[] spiderWalls = new bool[Main.maxTilesX * Main.maxTilesY];
-        public bool[] visitedCoords = new bool[Main.maxTilesX * Main.maxTilesY];
-        public List<List<Point16>> spiderCaves = new List<List<Point16>>();
+
+        public override void Initialize()
+        {
+            //Main.maxTilesX/Y change from world to world and needs to be updated
+            iterationsPerTick = Main.maxTilesX * Main.maxTilesY / 54000;
+        }
 
         public override TagCompound Save()
         {
@@ -32,6 +34,7 @@ namespace LivingWorldMod
             villageReputation = tag.GetIntArray("VillageReputation");
         }
 
+        //https://github.com/tModLoader/tModLoader/wiki/Vanilla-World-Generation-Steps
         public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)
         {
             int spiderCavesIndex = tasks.FindIndex(task => task.Name.Equals("Spider Caves"));
@@ -45,91 +48,103 @@ namespace LivingWorldMod
             progress.Message = "Spawning Extra Spider Caves Tiles";
             progress.Start(0f);
 
-            spiderCaves = new List<List<Point16>>();
+            int tileArray1D = Main.maxTilesX * Main.maxTilesY;
 
-            for (int j = 0; j < Main.maxTilesY; j++)
+            for (int k = 0; k < tileArray1D; k++)
             {
-                for (int i = 0; i < Main.maxTilesX; i++)
-                {
-                    if (i == Main.maxTilesX / 2 && j == Main.maxTilesY) progress.Start(.25f);
-                    Tile tile = Framing.GetTileSafely(i, j);
-                    //Using a 1D array to store values that correspond to 2D coordinates
-                    spiderWalls[j * Main.maxTilesX + i] = tile.wall == WallID.SpiderUnsafe;
+                progress.Set((float)k / tileArray1D);
 
-                    if (tile.wall == WallID.SpiderUnsafe && !tile.active() && !Framing.GetTileSafely(i + 1, j).active()
-                        && !Framing.GetTileSafely(i, j + 1).active() && !Framing.GetTileSafely(i + 1, j + 1).active())
-                    {
-                        //Coord must have a 5x2 empty space below them
-                        int height = 5;
-                        bool spaceBelow = true;
-                        for (int l = 0; l < 2; l++)
-                            for (int k = j; k < j + height; k++)
-                                if (Framing.GetTileSafely(i + l, k).active())
-                                    spaceBelow = false;
+                int i = k % Main.maxTilesX;
+                int j = k / Main.maxTilesX;
 
-                        if (spaceBelow)
-                        {
-                            //Prevents tiles in a square of 60 tiles of lenght where current coord is the center
-                            int radius = 30;
-                            bool foundInRadius = false;
-
-                            for (int m = j - radius; m < j + radius; m++) //y
-                            {
-                                if (foundInRadius) break;
-                                for (int n = i - radius; n < i + radius; n++) //x
-                                    if (Framing.GetTileSafely(n, m).type == TileType<SpiderSacTile>())
-                                    { foundInRadius = true; break; }
-                            }
-
-                            if (!foundInRadius)
-                                WorldGen.PlaceTile(i, j, TileType<SpiderSacTile>());
-                        }
-                    }
-                }
+                if (CanSpawnSpiderSac(i, j))
+                    WorldGen.PlaceTile(i, j, TileType<SpiderSacTile>(), forced: true);
             }
 
-            progress.Set(.5f);
-            //After one world iteration we know which coords have SpiderWalls on them
-            //and we can easily check if those coords were visited already by the BFS algorithm
-            for (int k = 0; k < spiderWalls.Length; k++)
-            {
-                if (k == spiderWalls.Length / 2) progress.Set(.75f);
-                if (!spiderWalls[k] || visitedCoords[k]) continue;
-
-                int x = k % Main.maxTilesX;
-                int y = k / Main.maxTilesX;
-
-                List<Point16> currentCave = new List<Point16>();
-                Point16 currentPos = new Point16(x, y);
-
-                //Simple BFS Algorithm implementation
-                Queue<TileNode> queue = new Queue<TileNode>();
-                TileNode startingNode = new TileNode(currentPos);
-                queue.Enqueue(startingNode);
-
-                while (queue.Count != 0)
-                {
-                    TileNode activeNode = queue.Dequeue();
-                    //1D array that holds values of 2D coordinates needs a special formula for the index
-                    int index = activeNode.position.Y * Main.maxTilesX + activeNode.position.X;
-
-                    if (visitedCoords[index]) continue;
-                    else visitedCoords[index] = true;
-
-                    //If current coord wasn't visited yet, add it to the current cave list
-                    currentCave.Add(activeNode.position);
-
-                    //Node's children already filter out tiles that don't have SpiderWalls as walls
-                    //allowing us to just add them all to the queue
-                    List<TileNode> childNodes = activeNode.GetChildren();
-                    childNodes.ForEach(child => queue.Enqueue(child));
-                }
-    
-                //Finish cave iteration
-                spiderCaves.Add(currentCave);
-            }
-
+            progress.Set(1f);
             progress.End();
+        }
+
+        private bool CanSpawnSpiderSac(int i, int j)
+        {
+            //Making sure we don't go out of world bounds
+            if (i > Main.maxTilesX || i - 1 < 0 || j > Main.maxTilesY || j - 1 < 0) return false;
+
+            Point16[] tileCoords = {
+                    //Top Left/Right, Mid Left/Right, Bottom Left/Right
+                    new Point16(i, j - 1), new Point16(i + 1, j - 1),
+                    new Point16(i, j), new Point16(i + 1, j),
+                    new Point16(i, j + 1), new Point16(i + 1, j + 1)
+                };
+
+            //Making sure all of the tiles are within a Spider Cave
+            foreach (Point16 coord in tileCoords)
+                if (Framing.GetTileSafely(coord).wall != WallID.SpiderUnsafe) return false;
+
+            //Coord must have a heightx2 empty space below them
+            int height = 7;
+            for (int l = 0; l < 2; l++)
+                for (int k = j; k < j + height; k++)
+                {
+                    Tile tempTile = Framing.GetTileSafely(i + l, k);
+                    if (tempTile.active() && (tempTile.type != TileID.Cobweb || tempTile.type != 159)) return false;
+                }
+
+            //Prevents tiles in a square of 60 tiles of lenght where current coord is the center
+            int radius = 30;
+            for (int m = j - radius; m < j + radius; m++) //y
+                for (int n = i - radius; n < i + radius; n++) //x
+                    if (Framing.GetTileSafely(n, m).type == TileType<SpiderSacTile>())
+                        return false;
+
+            //Making sure the foundation for the Spider Sac aren't cobwebs or other non solid stuff
+            for (int m = 0; m < 2; m++)
+                if (Framing.GetTileSafely(tileCoords[m]).collisionType != 1) 
+                    return false;
+
+            //Smoothing top tiles where Spider Sac will sit
+            for (int m = 0; m < 2; m++)
+            {
+                Framing.GetTileSafely(tileCoords[m]).slope(0);
+                Framing.GetTileSafely(tileCoords[m]).halfBrick(false);
+            }
+
+            //Removing Cobwebs where Spider Sac will be placed
+            for (int m = 2; m < 6; m++)
+                WorldGen.KillTile(tileCoords[m].X, tileCoords[m].Y, effectOnly: true);
+
+            return true;
+        }
+
+        int iterationsPerTick = 0;
+        int startingIteration = 0;
+        int currentIteration = 0;
+        int spiderSacsPlaced = 0;
+        public override void PostUpdate()
+        {
+            //Progressive iteration of the world throughout the span of 15mins
+            for (int i = 0; i < iterationsPerTick; i++)
+            {
+                //Resetting currentIteration to 0 after it reaches the max index
+                if (currentIteration >= Main.maxTilesX * Main.maxTilesY)
+                    currentIteration = spiderSacsPlaced = 0;
+
+                //After a full "lap" around the world we can set a new random point to start our "lap"
+                if (currentIteration == startingIteration)
+                    startingIteration = currentIteration = Main.rand.Next(Main.maxTilesX * Main.maxTilesY);
+
+                //Only allow a max of 2 Spider Sacs placed per "lap"
+                if (spiderSacsPlaced >= 2)
+                { currentIteration += iterationsPerTick - i; break; }
+
+                int x = currentIteration % Main.maxTilesX;
+                int y = currentIteration / Main.maxTilesX;
+
+                if (CanSpawnSpiderSac(x, y) && WorldGen.PlaceTile(x, y, TileType<SpiderSacTile>(), forced: true))
+                    spiderSacsPlaced++;
+
+                currentIteration++;
+            }
         }
     }
 }
--- Utils/LWMUtils.cs
+++ Utils/LWMUtils.cs
@@ -13,9 +13,5 @@ namespace LivingWorldMod.Utils
                 weightedRandom.Add(curType, weight);
             }
         }
-
-        //----------Extension Methods----------
-        public static Tile ToTile(this TileNode tn) => Framing.GetTileSafely(tn.position);
-        public static Point16 Add(this Point16 point, int p1x, int p1y) => point + new Point16(p1x, p1y);
     }
 }
