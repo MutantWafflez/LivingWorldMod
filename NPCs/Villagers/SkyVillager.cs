ae0caef4aab1040da6f948a900eb5d9857651d40
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2020-09-26 23:44:21 -0700

    Harpy Villager file reorganization

--- NPCs/Villagers/SkyVillager.cs
+++ NPCs/Villagers/SkyVillager.cs
@@ -1,4 +1,5 @@
-﻿using LivingWorldMod.Utils;
+﻿using LivingWorldMod.Items.Placeable.Paintings;
+using LivingWorldMod.Utils;
 using System.Collections.Generic;
 using System.IO;
 using Terraria;
@@ -22,6 +23,100 @@ namespace LivingWorldMod.NPCs.Villagers
             Main.npcFrameCount[npc.type] = 27;
         }
 
+        #region Villager Class Overrides
+        public override WeightedRandom<string> GetDialogueText()
+        {
+            WeightedRandom<string> chat = new WeightedRandom<string>();
+
+            if (isNegativeRep || Main.bloodMoon)
+            {
+                chat.Add("Don’t take it personally if we like, rip off your arms and eat you or something.");
+                chat.Add("How do you even live with those repulsive meat sticks in place of wings?");
+                chat.Add("You really ruffle my feathers, landwalker!");
+                chat.Add("How’s the weather down there? I hope it’s horrible.");
+                chat.Add("Leave me alone you ugly landwalking son of a- squak!");
+                chat.Add("Do you want my feathers going down your back like a stegosaurus?", 0.66);
+                chat.Add("Are all the flightless this fowl and annoying...?");
+                chat.Add("Humans... Despicable. Created those foul windows!");
+            }
+            else if (isNeutralRep)
+            {
+                chat.Add("I don’t get it, what’s so wrong with being bird-brained?");
+                chat.Add("No, I don’t want any crackers. And stop calling me Polly!", 0.66);
+                chat.Add("I hope I can be at the front of the V formation at least once before I die.");
+                chat.Add("Flying through the sky is like, so freeing. You should try it sometime.");
+                chat.Add("You’re asking how we reproduce if we’re all female? Don’t be silly.", 0.33);
+                chat.Add("I’m sort of scared of flying too high and floating into space or the sun or something.");
+                chat.Add("You’ll buy something, won’t you? You didn’t come here for nothing, right?");
+            }
+            else if (isPositiveRep || isMaxRep)
+            {
+                chat.Add(Main.LocalPlayer.name + "! You want to buy something, right? Or did you just want to talk to me?");
+                chat.Add("Aren’t my feathers pretty today? I spent all day grooming them.");
+                chat.Add("Does this top make my wings look fat?", 0.66);
+                chat.Add("You should sit with me on my perch sometime. The breeze is so nice, it really helps my zen.");
+                chat.Add("You aren’t so bad for a landwalking human. Come visit again sometime!");
+                chat.Add("It gets sort of boring up here. But it’s not so bad when you come around.");
+                chat.Add("ptoo- Sorry, I got a feather in my mouth again. Happens every time.", 0.66);
+                chat.Add("No I’m not flirting with you! Stupid human!", 0.33);
+            }
+            //General Event Chat
+
+            //Rain
+            chat.ConditionalStringAdd("I’m super thankful we’re above the clouds. I cannot stand my hair being wet.", Main.raining, 2);
+            chat.ConditionalStringAdd("Ahh, watching the rain fall from the clouds is so relaxing.", Main.raining, 2);
+            //Solar Eclipse
+            chat.ConditionalStringAdd("It’s so fun watching all the monsters going crazy down there attacking people. I’m glad we’re somewhat safe up here.", Main.eclipse, 2);
+            chat.ConditionalStringAdd("What, are you scared or something?", Main.eclipse, 2);
+
+            return chat;
+        }
+
+        public override WeightedRandom<string> GetReputationText()
+        {
+            WeightedRandom<string> chat = new WeightedRandom<string>();
+
+            chat.ConditionalStringAdd("You totally suck. You’d better make things better, or you’ll be ripped apart limb by limb the next time you even set foot here!", isNegativeRep);
+            chat.ConditionalStringAdd("I don’t have much of an opinion on you. You’re alright I guess, I don’t mind you being here.", isNeutralRep);
+            chat.ConditionalStringAdd("Hey, you aren’t so bad for a human. I hope you’ll visit us more often, we don’t have much to do up here in the sky anyway.", isPositiveRep);
+            chat.ConditionalStringAdd("The village loves you! It sucks that you aren’t a Harpy, but we don’t mind. Your charisma definitely compensates for it, plus, you’re like, pretty cute for a human. What? I didn’t say anything.", isMaxRep);
+
+            return chat;
+        }
+
+        public override List<string> GetPossibleNames()
+        {
+            List<string> names = new List<string>
+            {
+                "Merel",
+                "Mari",
+                "Wren",
+                "Yona",
+                "Jena",
+                "Tori",
+                "Loa",
+                "Eve",
+                "Rima",
+                "Luyu",
+                "Robin"
+            };
+            return names;
+        }
+
+        public override bool CanChat() => !isFlying;
+        #endregion
+
+        #region Normal Overrides
+        public override void SetupShop(Chest shop, ref int nextSlot)
+        {
+            shop.item[nextSlot].SetDefaults(ModContent.ItemType<SkyBustTileItem>());
+            nextSlot++;
+            shop.item[nextSlot].SetDefaults(ModContent.ItemType<OneStarTileItem>());
+            nextSlot++;
+        }
+        #endregion
+
+        #region Animation & AI
         public override void AI()
         {
             //This check is IMPORTANT. We don't want to use PreAI cause that obstructs functionality of some other mods,
@@ -125,89 +220,6 @@ namespace LivingWorldMod.NPCs.Villagers
             }
         }
 
-        #region Villager Class Overrides
-        public override WeightedRandom<string> GetDialogueText()
-        {
-            WeightedRandom<string> chat = new WeightedRandom<string>();
-
-            if (isNegativeRep || Main.bloodMoon)
-            {
-                chat.Add("Don’t take it personally if we like, rip off your arms and eat you or something.");
-                chat.Add("How do you even live with those repulsive meat sticks in place of wings?");
-                chat.Add("You really ruffle my feathers, landwalker!");
-                chat.Add("How’s the weather down there? I hope it’s horrible.");
-                chat.Add("Leave me alone you ugly landwalking son of a- squak!");
-                chat.Add("Do you want my feathers going down your back like a stegosaurus?", 0.66);
-                chat.Add("Are all the flightless this fowl and annoying...?");
-                chat.Add("Humans... Despicable. Created those foul windows!");
-            }
-            else if (isNeutralRep)
-            {
-                chat.Add("I don’t get it, what’s so wrong with being bird-brained?");
-                chat.Add("No, I don’t want any crackers. And stop calling me Polly!", 0.66);
-                chat.Add("I hope I can be at the front of the V formation at least once before I die.");
-                chat.Add("Flying through the sky is like, so freeing. You should try it sometime.");
-                chat.Add("You’re asking how we reproduce if we’re all female? Don’t be silly.", 0.33);
-                chat.Add("I’m sort of scared of flying too high and floating into space or the sun or something.");
-                chat.Add("You’ll buy something, won’t you? You didn’t come here for nothing, right?");
-            }
-            else if (isPositiveRep || isMaxRep)
-            {
-                chat.Add(Main.LocalPlayer.name + "! You want to buy something, right? Or did you just want to talk to me?");
-                chat.Add("Aren’t my feathers pretty today? I spent all day grooming them.");
-                chat.Add("Does this top make my wings look fat?", 0.66);
-                chat.Add("You should sit with me on my perch sometime. The breeze is so nice, it really helps my zen.");
-                chat.Add("You aren’t so bad for a landwalking human. Come visit again sometime!");
-                chat.Add("It gets sort of boring up here. But it’s not so bad when you come around.");
-                chat.Add("ptoo- Sorry, I got a feather in my mouth again. Happens every time.", 0.66);
-                chat.Add("No I’m not flirting with you! Stupid human!", 0.33);
-            }
-            //General Event Chat
-
-            //Rain
-            chat.ConditionalStringAdd("I’m super thankful we’re above the clouds. I cannot stand my hair being wet.", Main.raining, 2);
-            chat.ConditionalStringAdd("Ahh, watching the rain fall from the clouds is so relaxing.", Main.raining, 2);
-            //Solar Eclipse
-            chat.ConditionalStringAdd("It’s so fun watching all the monsters going crazy down there attacking people. I’m glad we’re somewhat safe up here.", Main.eclipse, 2);
-            chat.ConditionalStringAdd("What, are you scared or something?", Main.eclipse, 2);
-
-            return chat;
-        }
-
-        public override WeightedRandom<string> GetReputationText()
-        {
-            WeightedRandom<string> chat = new WeightedRandom<string>();
-
-            chat.ConditionalStringAdd("You totally suck. You’d better make things better, or you’ll be ripped apart limb by limb the next time you even set foot here!", isNegativeRep);
-            chat.ConditionalStringAdd("I don’t have much of an opinion on you. You’re alright I guess, I don’t mind you being here.", isNeutralRep);
-            chat.ConditionalStringAdd("Hey, you aren’t so bad for a human. I hope you’ll visit us more often, we don’t have much to do up here in the sky anyway.", isPositiveRep);
-            chat.ConditionalStringAdd("The village loves you! It sucks that you aren’t a Harpy, but we don’t mind. Your charisma definitely compensates for it, plus, you’re like, pretty cute for a human. What? I didn’t say anything.", isMaxRep);
-
-            return chat;
-        }
-
-        public override List<string> GetPossibleNames()
-        {
-            List<string> names = new List<string>
-            {
-                "Merel",
-                "Mari",
-                "Wren",
-                "Yona",
-                "Jena",
-                "Tori",
-                "Loa",
-                "Eve",
-                "Rima",
-                "Luyu",
-                "Robin"
-            };
-            return names;
-        }
-
-        public override bool CanChat() => !isFlying;
-        #endregion
-
         public override void FindFrame(int frameHeight)
         {
             //Flying animation should take place when flying, obviously, and when the Harpy is adjusting against Gravity when first taking off
@@ -233,5 +245,6 @@ namespace LivingWorldMod.NPCs.Villagers
                 }
             }
         }
+        #endregion
     }
 }
