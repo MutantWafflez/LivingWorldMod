ec9f2e52c82c8e9fcb3812bc366f8e0edb5d3705
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2020-10-11 18:43:18 -0700

    Harpy Villagers immune to Hostile Harpies

--- NPCs/LWMGeneralGlobalNPC.cs
+++ NPCs/LWMGeneralGlobalNPC.cs
@@ -1,4 +1,5 @@
 ﻿using LivingWorldMod.NPCs.Villagers;
+using LivingWorldMod.Utils;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
@@ -25,6 +26,32 @@ namespace LivingWorldMod.NPCs
             return base.SpecialNPCLoot(npc);
         }
 
+        /// <summary>
+        /// Prevents hostile harpies or wyverns from hitting Harpy Villagers.
+        /// </summary>
+        public override bool? CanHitNPC(NPC npc, NPC target) {
+            if ((npc.type == NPCID.Harpy ||
+                npc.type == NPCID.WyvernHead ||
+                npc.type == NPCID.WyvernBody ||
+                npc.type == NPCID.WyvernBody2 ||
+                npc.type == NPCID.WyvernBody3 ||
+                npc.type == NPCID.WyvernLegs ||
+                npc.type == NPCID.WyvernTail)
+                && target.type == ModContent.NPCType<SkyVillager>()) {
+                return false;
+            }
+            return base.CanHitNPC(npc, target);
+        }
+
+        public override void PostAI(NPC npc) {
+            //Prevent any Hostile Harpies from being near the Harpy Village
+            if (npc.type == NPCID.Harpy) {
+                if (Main.netMode != NetmodeID.MultiplayerClient && npc.Distance(npc.FindNearestNPC(ModContent.NPCType<SkyVillager>()).Center) <= 16 * 30) {
+                    npc.active = false;
+                    npc.netUpdate = true;
+                }
+            }
+        }
 
     }
 }
