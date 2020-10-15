9aefaa8e06ee4f30e24e03af928ad1af998ccbd1
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2020-10-14 23:54:44 -0700

    GiftPreferences Custom Reputation
    To be more descriptive: instead each item of liked gifts giving the same reputation and likewise for disliked gifts, each item in the list can have its own custom reputation modification value

--- NPCs/Villagers/GiftPreferences.cs
+++ NPCs/Villagers/GiftPreferences.cs
@@ -5,50 +5,51 @@ using Terraria.ID;
 
 namespace LivingWorldMod.NPCs.Villagers {
     public struct GiftPreferences {
-        private List<int> likedGifts;
+        private List<Tuple<short, int>> likedGifts;
 
-        private List<int> dislikedGifts;
+        private List<Tuple<short, int>> dislikedGifts;
 
-        public GiftPreferences(List<int> likedGifts, List<int> dislikedGifts) {
+        public GiftPreferences(List<Tuple<short, int>> likedGifts, List<Tuple<short, int>> dislikedGifts) {
             this.likedGifts = likedGifts;
             this.dislikedGifts = dislikedGifts;
         }
 
         /// <summary>
-        /// Returns whether or not item of a given type is contained within the likedGifts list.
+        /// Searches both the likedGifts and dislikedGifts list for itemType, and returns the number it modifies Reputation by.
         /// </summary>
-        /// <param name="itemType">The type of the item to look for.</param>
-        public bool LikedGiftsContains(int itemType) {
-            return likedGifts.Contains(itemType);
-        }
+        /// <param name="itemType">The type of the item being searched.</param>
+        public int GetReputationModifier(short itemType) {
+            int reputationModification = 0;
 
-        /// <summary>
-        /// Returns whether or not item of a given type is contained within the dislikedGifts list.
-        /// </summary>
-        /// <param name="itemType">The type of the item to look for.</param>
-        public bool DislikedGiftsContains(int itemType) {
-            return dislikedGifts.Contains(itemType);
+            var likedResult = likedGifts.FirstOrDefault(gift => gift.Item1 == itemType);
+            reputationModification = likedResult != null ? likedResult.Item2 : reputationModification;
+
+            var dislikedResult = dislikedGifts.FirstOrDefault(gift => gift.Item1 == itemType);
+            reputationModification = dislikedResult != null ? dislikedResult.Item2 : reputationModification;
+
+            return reputationModification;
         }
 
         public static GiftPreferences[] InitializeGiftArray() {
             return new GiftPreferences[(int)VillagerType.VillagerTypeCount] 
             {
                 //Harpy
-                new GiftPreferences(new List<int> 
+                new GiftPreferences(new List<Tuple<short, int>> 
                 {
-                    ItemID.FallenStar,
-                    ItemID.Worm
+                    Tuple.Create(ItemID.FallenStar, 5),
+                    Tuple.Create(ItemID.Worm, 3)
                 },
-                new List<int> {
-                    ItemID.Feather
+                new List<Tuple<short, int>> {
+                    Tuple.Create(ItemID.Feather, -3),
+                    Tuple.Create(ItemID.GiantHarpyFeather, -5)
                 }
                 ),
                 //Lihzahrd
-                new GiftPreferences(new List<int> 
+                new GiftPreferences(new List<Tuple<short, int>>
                 {
 
                 }, 
-                new List<int> 
+                new List<Tuple<short, int>>
                 {
 
                 }
