4023a5476bb698cb5ea2d075feb680e96c6b9e3c
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-06-10 15:26:28 -0700

    Create WorldCreationSystem.cs

---
+++ Common/Systems/WorldCreationSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Terraria.ModLoader;
+using Terraria.WorldBuilding;
+
+namespace LivingWorldMod.Common.Systems {
+
+    /// <summary>
+    /// System that handles the INITIAL world generation steps. This system does NOT handle world
+    /// events that occur AFTER the world is created.
+    /// </summary>
+    public class WorldCreationSystem : ModSystem {
+
+        public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight) {
+        }
+    }
+}
