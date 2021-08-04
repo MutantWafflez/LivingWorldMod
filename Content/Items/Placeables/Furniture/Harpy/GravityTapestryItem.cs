3125e63f91f277f0bd60f1fc7eb0c2fac2217cd4
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-08-04 13:23:45 -0700

    Begin Creation of Villager Tiles

---
+++ Content/Items/Placeables/Furniture/Harpy/GravityTapestryItem.cs
@@ -0,0 +1,15 @@
+﻿using LivingWorldMod.Content.Tiles.Furniture.Harpy;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace LivingWorldMod.Content.Items.Placeables.Furniture.Harpy {
+    public class GravityTapestryItem : BaseItem {
+        public override void SetDefaults() {
+            Item.CloneDefaults(ItemID.BlueBanner);
+            Item.rare = ItemRarityID.Blue;
+            Item.placeStyle = 0;
+            Item.value = Terraria.Item.buyPrice(silver: 20);
+            Item.createTile = ModContent.TileType<GravityTapestryTile>();
+        }
+    }
+}
---
+++ Content/Tiles/Furniture/Harpy/GravityTapestryTile.cs
@@ -0,0 +1,32 @@
+﻿using LivingWorldMod.Content.Items.Placeables.Furniture.Harpy;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace LivingWorldMod.Content.Tiles.Furniture.Harpy {
+    public class GravityTapestryTile : BaseTile {
+        public override void SetStaticDefaults() {
+            Main.tileSolid[Type] = false;
+            Main.tileNoSunLight[Type] = false;
+            Main.tileNoAttach[Type] = true;
+            Main.tileWaterDeath[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x2Top);
+            TileObjectData.newTile.Width = 3;
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.Origin = new Point16(1, 0);
+            TileObjectData.newTile.CoordinateHeights = new int[] {16, 16, 16};
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(Color.LightBlue);
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY) {
+            Item.NewItem(i * 16, j * 16, 48, 48, ModContent.ItemType<GravityTapestryItem>());
+        }
+    }
+}
