b29554ac998a8708674d521e20767da74ab05cc3
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-08-04 16:14:44 -0700

    Skyware Anvil

---
+++ Content/Items/Placeables/Furniture/Harpy/SkywareAnvilItem.cs
@@ -0,0 +1,16 @@
+﻿using LivingWorldMod.Content.Tiles.Furniture.Harpy;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace LivingWorldMod.Content.Items.Placeables.Furniture.Harpy {
+    public class SkywareAnvilItem : BaseItem{
+
+        public override void SetDefaults() {
+            Item.CloneDefaults(ItemID.IronAnvil);
+            Item.placeStyle = 0;
+            Item.value = Terraria.Item.buyPrice(silver: 5);
+            Item.createTile = ModContent.TileType<SkywareAnvilTile>();
+        }
+
+    }
+}
---
+++ Content/Tiles/Furniture/Harpy/SkywareAnvilTile.cs
@@ -0,0 +1,36 @@
+﻿using LivingWorldMod.Content.Items.Placeables.Furniture.Harpy;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace LivingWorldMod.Content.Tiles.Furniture.Harpy {
+    public class SkywareAnvilTile : BaseTile {
+        public override void SetStaticDefaults() {
+            Main.tileSolid[Type] = false;
+            Main.tileNoSunLight[Type] = false;
+            Main.tileSolidTop[Type] = true;
+            Main.tileWaterDeath[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1);
+            TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile, TileObjectData.newTile.Width, 0);
+            TileObjectData.addTile(Type);
+
+            AdjTiles = new int[] {TileID.Anvils};
+
+            ModTranslation mapName = CreateMapEntryName();
+            mapName.SetDefault("Skyware Anvil");
+
+            AddMapEntry(Color.LightBlue, mapName);
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY) {
+            Item.NewItem(i * 16, j * 16, 32, 16, ModContent.ItemType<SkywareAnvilItem>());
+        }
+    }
+}
