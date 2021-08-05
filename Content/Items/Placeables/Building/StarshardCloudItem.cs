d8641f3a690f15b983fce784d7c467baead5eb11
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-08-04 17:33:02 -0700

    Starshard Cloud Tile

---
+++ Content/Items/Placeables/Building/StarshardCloudItem.cs
@@ -0,0 +1,15 @@
+﻿using LivingWorldMod.Content.Tiles.Building;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace LivingWorldMod.Content.Items.Placeables.Building {
+    public class StarshardCloudItem : BaseItem {
+        public override void SetDefaults() {
+            Item.CloneDefaults(ItemID.DirtBlock);
+            Item.value = Item.buyPrice(copper: 75);
+            Item.placeStyle = 0;
+            Item.createTile = ModContent.TileType<StarshardCloudTile>();
+        }
+    }
+}
---
+++ Content/Tiles/Building/StarshardCloudTile.cs
@@ -0,0 +1,24 @@
+﻿using LivingWorldMod.Content.Items.Placeables.Building;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace LivingWorldMod.Content.Tiles.Building {
+    public class StarshardCloudTile : BaseTile {
+        public override void SetStaticDefaults() {
+            Main.tileSolid[Type] = true;
+            Main.tileNoSunLight[Type] = true;
+            Main.tileMergeDirt[Type] = false;
+
+            MineResist = 1.34f;
+
+            ItemDrop = ModContent.ItemType<StarshardCloudItem>();
+            DustType = DustID.Cloud;
+
+            AddMapEntry(Color.LightYellow);
+        }
+
+        public override bool HasWalkDust() => true;
+    }
+}
