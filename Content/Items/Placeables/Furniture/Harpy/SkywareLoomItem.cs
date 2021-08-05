4f26acc25bf8da5b4c8d3692914abf17209f9ff2
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-08-04 17:02:43 -0700

    Skyware Loom

---
+++ Content/Items/Placeables/Furniture/Harpy/SkywareLoomItem.cs
@@ -0,0 +1,16 @@
+﻿using LivingWorldMod.Content.Tiles.Furniture.Harpy;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace LivingWorldMod.Content.Items.Placeables.Furniture.Harpy {
+    public class SkywareLoomItem : BaseItem {
+
+        public override void SetDefaults() {
+            Item.CloneDefaults(ItemID.LivingLoom);
+            Item.placeStyle = 0;
+            Item.value = Terraria.Item.buyPrice(silver: 40);
+            Item.createTile = ModContent.TileType<SkywareLoomTile>();
+        }
+
+    }
+}
---
+++ Content/Tiles/Furniture/Harpy/SkywareLoomTile.cs
@@ -0,0 +1,38 @@
+﻿using Terraria.ID;
+using LivingWorldMod.Content.Items.Placeables.Furniture.Harpy;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace LivingWorldMod.Content.Tiles.Furniture.Harpy {
+    public class SkywareLoomTile : BaseTile {
+
+        public override void SetStaticDefaults() {
+            Main.tileSolid[Type] = false;
+            Main.tileNoSunLight[Type] = false;
+            Main.tileNoAttach[Type] = true;
+            Main.tileWaterDeath[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.Origin = new Point16(0, 2);
+            TileObjectData.addTile(Type);
+
+            AdjTiles = new int[] { TileID.Loom };
+
+            ModTranslation mapName = CreateMapEntryName();
+            mapName.SetDefault("Skyware Loom");
+
+            AddMapEntry(Color.FloralWhite);
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY) {
+            Item.NewItem(i * 16, j * 16, 32, 48, ModContent.ItemType<SkywareLoomItem>());
+        }
+    }
+
+}
