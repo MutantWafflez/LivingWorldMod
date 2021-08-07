f92c7fe952807fb5fcc365ea4c7a3d234936bf88
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-08-06 19:36:21 -0700

    Restock shop debug command

---
+++ Common/Commands/ShopRestockDebugCommand.cs
@@ -0,0 +1,42 @@
+﻿#if DEBUG
+using LivingWorldMod.Content.NPCs.Villagers;
+using LivingWorldMod.Custom.Enums;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace LivingWorldMod.Common.Commands {
+    public class ShopRestockDebugCommand : ModCommand {
+        public override string Command => "restock";
+
+        public override string Description => "Restocks all the shops of the given villager type.";
+
+        public override string Usage => "/restock <VillagerType>";
+
+        public override CommandType Type => CommandType.Chat;
+
+        public override void Action(CommandCaller caller, string input, string[] args)
+        {
+            if (args.Length == 1 && int.TryParse(args[0], out int type))
+            {
+                if (type >= 0 && type < (int)VillagerType.TypeCount)
+                {
+                    for (int i = 0; i < Main.maxNPCs; i++) {
+                        if (Main.npc[i].ModNPC is HarpyVillager villager) {
+                            villager.RestockShop();
+                        }
+                    }
+                    caller.Reply((VillagerType)type + " Villagers' shops successfully restocked!");
+                }
+                else
+                {
+                    throw new UsageException("Did not input correct Village Type: " + type);
+                }
+            }
+            else
+            {
+                throw new UsageException("Did not input a Village Type.");
+            }
+        }
+    }
+}
+#endif
