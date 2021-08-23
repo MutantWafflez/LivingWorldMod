342777f2f5a15191782e41ce30c3fadcd8d8b71f
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-08-22 18:12:04 -0700

    Harpy-based blocks now merge when placed together & with dirt

--- Content/Tiles/Building/StarshardCloudTile.cs
+++ Content/Tiles/Building/StarshardCloudTile.cs
@@ -12,8 +12,8 @@ namespace LivingWorldMod.Content.Tiles.Building {
         public override void SetStaticDefaults() {
             Main.tileSolid[Type] = true;
             Main.tileNoSunLight[Type] = true;
-            Main.tileMergeDirt[Type] = false;
             Main.tileLighted[Type] = true;
+            Main.tileMergeDirt[Type] = true;
             Main.tileMerge[Type][TileID.Cloud] = true;
             Main.tileMerge[Type][TileID.RainCloud] = true;
             Main.tileMerge[Type][TileID.SnowCloud] = true;
--- Content/Tiles/Building/StarshineBlockTile.cs
+++ Content/Tiles/Building/StarshineBlockTile.cs
@@ -11,7 +11,10 @@ namespace LivingWorldMod.Content.Tiles.Building {
         public override void SetStaticDefaults() {
             Main.tileSolid[Type] = true;
             Main.tileNoSunLight[Type] = true;
-            Main.tileMergeDirt[Type] = false;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileMerge[Type][ModContent.TileType<SunslabBlockTile>()] = true;
+            Main.tileMerge[Type][TileID.Sunplate] = true;
+            Main.tileMerge[TileID.Sunplate][Type] = true;
 
             MineResist = 1.34f;
 
