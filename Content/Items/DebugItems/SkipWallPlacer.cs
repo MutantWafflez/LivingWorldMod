e2f1c2f9ef5a9c37d787ab7b62ed957c26a16ec5
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-09-22 22:10:07 -0700

    Skip Wall Placer Tool

---
+++ Content/Items/DebugItems/SkipWallPlacer.cs
@@ -0,0 +1,82 @@
+﻿#if DEBUG
+
+using LivingWorldMod.Custom.Utilities;
+using System;
+using System.Collections.Generic;
+using LivingWorldMod.Content.Walls.DebugWalls;
+using LivingWorldMod.Custom.Structs;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace LivingWorldMod.Content.Items.DebugItems {
+
+    /// <summary>
+    /// Debug tool that can place debug walls
+    /// </summary>
+    public class SkipWallPlacer : BaseItem {
+        private Point16 topLeft = Point16.NegativeOne;
+
+        private Point16 bottomRight = Point16.NegativeOne;
+
+        private bool isPlacingWalls;
+
+        public override string Texture => "Terraria/Images/Item_" + ItemID.ActuationRod;
+
+        public override void SetDefaults() {
+            Item.CloneDefaults(ItemID.DrumStick);
+            Item.rare = ItemRarityID.Quest;
+            Item.autoReuse = false;
+            Item.useTime = 30;
+            Item.useAnimation = 30;
+        }
+
+        public override bool? UseItem(Player player) {
+            if (player.altFunctionUse == 0) {
+                if (topLeft == Point16.NegativeOne) {
+                    topLeft = new Point16((int)(Main.MouseWorld.X / 16f), (int)(Main.MouseWorld.Y / 16f));
+                    Main.NewText("Top Left Set to: " + topLeft.X + ", " + topLeft.Y);
+                }
+                else if (bottomRight == Point16.NegativeOne) {
+                    bottomRight = new Point16((int)(Main.MouseWorld.X / 16f), (int)(Main.MouseWorld.Y / 16f));
+                    Main.NewText("Bottom Right Set to: " + bottomRight.X + ", " + bottomRight.Y);
+                }
+                else {
+                    topLeft = Point16.NegativeOne;
+                    bottomRight = Point16.NegativeOne;
+                    Main.NewText("Points Reset");
+                }
+                return true;
+            }
+            else if (player.altFunctionUse == 2 && !isPlacingWalls && topLeft != Point16.NegativeOne && bottomRight != Point16.NegativeOne) {
+                Main.NewText("Placing Skip Walls...");
+                PlaceSkipWalls();
+                return true;
+            }
+
+            return null;
+        }
+
+        public override bool AltFunctionUse(Player player) => true;
+
+        private void PlaceSkipWalls() {
+            isPlacingWalls = true;
+
+            for (int x = 0; x <= bottomRight.X - topLeft.X; x++) {
+                for (int y = 0; y <= bottomRight.Y - topLeft.Y; y++) {
+                    Tile requestedTile = Framing.GetTileSafely(x + topLeft.X, y + topLeft.Y);
+                    if (requestedTile.wall == WallID.None) {
+                        WorldGen.PlaceWall(x + topLeft.X, y + topLeft.Y, ModContent.WallType<SkipWall>());
+                    }
+                }
+            }
+
+            isPlacingWalls = false;
+            Main.NewText("Walls Placed!");
+        }
+    }
+}
+
+#endif
