f70fc38e7fa356cdc03a134d460dba8cfe322f19
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-10-07 21:43:38 -0700

    Start of Villager Housing Menu

---
+++ Common/Systems/UI/VillagerHousingUISystem.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using LivingWorldMod.Content.UI;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.UI;
+
+namespace LivingWorldMod.Common.Systems.UI {
+
+    /// <summary>
+    /// System that handles the initialization and opening/closing of the Villager Housing UI.
+    /// </summary>
+    [Autoload(Side = ModSide.Client)]
+    public class VillagerHousingUISystem : ModSystem {
+        public UserInterface housingInterface;
+        public VillagerHousingUIState housingState;
+        public GameTime lastGameTime;
+
+        public override void Load() {
+            housingInterface = new UserInterface();
+            housingState = new VillagerHousingUIState();
+
+            housingState.Activate();
+
+            housingInterface.SetState(housingState);
+        }
+
+        public override void Unload() {
+            housingInterface = null;
+            housingState = null;
+
+            lastGameTime = null;
+        }
+
+        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers) {
+            int inventoryIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Inventory"));
+            if (inventoryIndex != -1) {
+                layers.Insert(inventoryIndex + 1, new LegacyGameInterfaceLayer(
+                    "LWM: Villager Housing Interface",
+                    delegate {
+                        if (lastGameTime != null && housingInterface?.CurrentState != null) {
+                            housingInterface.Draw(Main.spriteBatch, lastGameTime);
+                        }
+                        return true;
+                    },
+                    InterfaceScaleType.UI));
+            }
+        }
+
+        public override void UpdateUI(GameTime gameTime) {
+            lastGameTime = gameTime;
+            if (housingInterface?.CurrentState != null) {
+                housingInterface.Update(gameTime);
+            }
+        }
+    }
+}
---
+++ Content/UI/VillagerHousingUIState.cs
@@ -0,0 +1,31 @@
+﻿using LivingWorldMod.Content.UI.Elements;
+using LivingWorldMod.Custom.Enums;
+using Terraria.UI;
+
+namespace LivingWorldMod.Content.UI {
+
+    /// <summary>
+    /// UIState that handles the visuals of the Housing UI.
+    /// </summary>
+    public class VillagerHousingUIState : UIState {
+
+        /// <summary>
+        /// Whether or not the menu that shows each of the villagers is visible (open).
+        /// </summary>
+        public bool isMenuVisible;
+
+        /// <summary>
+        /// The Villager type to currently be showing to the player.
+        /// </summary>
+        public VillagerType typeToShow;
+
+        /// <summary>
+        /// The button that closes/opens the menu showing each of the villagers.
+        /// </summary>
+        public UIBetterImageButton openMenuButton;
+
+        public override void OnInitialize() {
+            typeToShow = VillagerType.Harpy;
+        }
+    }
+}
