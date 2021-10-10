e90ea8935c6842dfe4ad0f2dbba6b632537f1d5b
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-10-09 19:23:08 -0700

    Hovering Functionality & ImmediateLoad fixes

--- Content/UI/Elements/UIBetterImageButton.cs
+++ Content/UI/Elements/UIBetterImageButton.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using Terraria;
@@ -26,17 +27,26 @@ namespace LivingWorldMod.Content.UI.Elements {
         private string text;
 
         private Asset<Texture2D> buttonTexture;
+
         private Asset<Texture2D> borderTexture;
 
         private float activeVisibility = 1f;
+
         private float inactiveVisibility = 0.4f;
 
+        /// <summary>
+        /// Simple action/event that triggers after every frame while the mouse is currently
+        /// hovering over this element.
+        /// </summary>
+        public event Action WhileHovering;
+
+        //Remember to use ImmediateLoad request mode if you request the texture in the parameter!
         public UIBetterImageButton(Asset<Texture2D> buttonTexture, string text = "", float textSize = 1f) {
             this.buttonTexture = buttonTexture;
             this.text = text;
             this.textSize = textSize;
-            Width.Set(buttonTexture.Width(), 0f);
-            Height.Set(buttonTexture.Height(), 0f);
+            Width.Set(buttonTexture.Value.Width, 0f);
+            Height.Set(buttonTexture.Value.Height, 0f);
         }
 
         public override void OnInitialize() {
@@ -47,8 +57,9 @@ namespace LivingWorldMod.Content.UI.Elements {
             buttonText = new UIBetterText(text, textSize) {
                 HAlign = 0.5f,
                 VAlign = 0.5f,
-                horizontalTextConstraint = buttonTexture.Width()
+                horizontalTextConstraint = buttonTexture.Value.Width
             };
+
             Append(buttonText);
         }
 
@@ -76,25 +87,31 @@ namespace LivingWorldMod.Content.UI.Elements {
             SoundEngine.PlaySound(SoundID.MenuTick);
         }
 
-        public override void MouseOut(UIMouseEvent evt) => base.MouseOut(evt);
-
         public void SetVisibility(float whenActive, float whenInactive) {
             activeVisibility = MathHelper.Clamp(whenActive, 0.0f, 1f);
             inactiveVisibility = MathHelper.Clamp(whenInactive, 0.0f, 1f);
         }
 
         protected override void DrawSelf(SpriteBatch spriteBatch) {
-            buttonText.isVisible = isVisible;
+            if (buttonText is not null) {
+                buttonText.isVisible = isVisible;
+            }
+
             if (!isVisible) {
                 return;
             }
 
             CalculatedStyle dimensions = GetDimensions();
             spriteBatch.Draw(buttonTexture.Value, dimensions.Position(), Color.White * (IsMouseHovering ? activeVisibility : inactiveVisibility));
-            if (borderTexture == null || !IsMouseHovering) {
+            if (!IsMouseHovering) {
                 return;
             }
-            spriteBatch.Draw(borderTexture.Value, dimensions.Position(), Color.White);
+
+            WhileHovering.Invoke();
+
+            if (borderTexture != null) {
+                spriteBatch.Draw(borderTexture.Value, dimensions.Position(), Color.White);
+            }
         }
     }
 }
--- Content/UI/VillagerHousingUIState.cs
+++ Content/UI/VillagerHousingUIState.cs
@@ -1,5 +1,11 @@
 ﻿using LivingWorldMod.Content.UI.Elements;
 using LivingWorldMod.Custom.Enums;
+using LivingWorldMod.Custom.Utilities;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
 using Terraria.UI;
 
 namespace LivingWorldMod.Content.UI {
@@ -24,8 +30,55 @@ namespace LivingWorldMod.Content.UI {
         /// </summary>
         public UIBetterImageButton openMenuButton;
 
+        /// <summary>
+        /// The displacement of the menu icon based on the map and it's current mode/placement.
+        /// </summary>
+        private int mapDisplacement;
+
+        /// <summary>
+        /// Path to the sprites for this UI.
+        /// </summary>
+        private string HousingTexturePath => LivingWorldMod.LWMSpritePath + "/UI/VillagerHousingUI/";
+
         public override void OnInitialize() {
             typeToShow = VillagerType.Harpy;
+
+            openMenuButton = new UIBetterImageButton(ModContent.Request<Texture2D>(HousingTexturePath + "VillagerHousing_Off", AssetRequestMode.ImmediateLoad)) {
+                isVisible = false
+            };
+            openMenuButton.SetHoverImage(ModContent.Request<Texture2D>(HousingTexturePath + "VillagerHousing_On", AssetRequestMode.ImmediateLoad));
+            openMenuButton.SetVisibility(1f, 1f);
+            openMenuButton.WhileHovering += WhileHovering;
+
+            Append(openMenuButton);
+        }
+
+        protected override void DrawChildren(SpriteBatch spriteBatch) {
+            bool isMiniMapEnabled = !Main.mapFullscreen && Main.mapStyle == 1;
+
+            //Adapted vanilla code since "Main.mH" is private, and I do not want to use reflection every frame
+            mapDisplacement = 0;
+
+            if (Main.mapEnabled) {
+                if (!Main.mapFullscreen && Main.mapStyle == 1) {
+                    mapDisplacement = 256;
+                }
+
+                if (mapDisplacement + Main.instance.RecommendedEquipmentAreaPushUp > Main.screenHeight) {
+                    mapDisplacement = Main.screenHeight - Main.instance.RecommendedEquipmentAreaPushUp;
+                }
+            }
+
+            openMenuButton.isVisible = Main.playerInventory;
+
+            openMenuButton.Left.Set(Main.screenWidth - (isMiniMapEnabled ? 220f : 175f), 0f);
+            openMenuButton.Top.Set((isMiniMapEnabled ? 145f : 116f) + mapDisplacement, 0f);
+
+            base.DrawChildren(spriteBatch);
+        }
+
+        private void WhileHovering() {
+            Main.instance.MouseText(LocalizationUtilities.GetLWMTextValue("UI.VillagerHousing.ButtonHoverText"));
         }
     }
 }
