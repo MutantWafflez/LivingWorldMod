e877631e89f22110c53f457ad5a03b39e70ed332
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-11-03 08:07:01 -0700

    Triangles have arrived

--- Common/Systems/DebugSystems/WorldGenDebugSystem.cs
+++ Common/Systems/DebugSystems/WorldGenDebugSystem.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.Xna.Framework;
+﻿using LivingWorldMod.Common.VanillaOverrides.WorldGen.GenShapes;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.WorldBuilding;
 
 namespace LivingWorldMod.Common.Systems.DebugSystems {
 
@@ -23,6 +26,10 @@ namespace LivingWorldMod.Common.Systems.DebugSystems {
             Dust.QuickBox(new Vector2(x, y) * 16, new Vector2(x + 1, y + 1) * 16, 2, Color.YellowGreen, null);
 
             // Code to test placed here:
+
+            Point origin = new Point(x, y);
+
+            WorldUtils.Gen(origin, new EqualTriangle(271), new Actions.SetTile(TileID.SandStoneSlab, true));
         }
     }
 }
