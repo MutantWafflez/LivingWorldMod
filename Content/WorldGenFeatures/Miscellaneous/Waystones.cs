b9bb42ce0242750611ab2b2ce3ca02c35bc490fc
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2021-11-17 18:27:26 -0800

    Waystone WorldGen pt. I

---
+++ Content/WorldGenFeatures/Miscellaneous/Waystones.cs
@@ -0,0 +1,9 @@
+﻿using LivingWorldMod.Common.ModTypes;
+
+namespace LivingWorldMod.Content.WorldGenFeatures.Miscellaneous {
+    public class Waystones : WorldGenFeature {
+        public override string InternalGenerationName => "Waystones";
+
+        public override string InsertionPassNameForFeature => "Micro Biomes";
+    }
+}
