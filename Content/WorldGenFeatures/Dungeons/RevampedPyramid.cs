82b5f53e7dc773b47d49aec68dd6607e6ab5b1a3
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2022-04-26 16:33:30 -0700

    Overworld Pyramid gen testing

--- Content/WorldGenFeatures/Dungeons/RevampedPyramid.cs
+++ Content/WorldGenFeatures/Dungeons/RevampedPyramid.cs
@@ -1,6 +1,11 @@
 ﻿using System.Collections.Generic;
+using Terraria;
 using LivingWorldMod.Common.ModTypes;
+using LivingWorldMod.Custom.Structs;
+using LivingWorldMod.Custom.Utilities;
+using Microsoft.Xna.Framework;
 using Terraria.GameContent.Generation;
+using Terraria.ID;
 using Terraria.IO;
 using Terraria.ModLoader;
 using Terraria.WorldBuilding;
@@ -37,6 +42,26 @@ namespace LivingWorldMod.Content.WorldGenFeatures.Dungeons {
         public override void Generate(GenerationProgress progress, GameConfiguration gameConfig) {
             progress.Message = "Cursing the Pyramid";
             progress.Set(0f);
+
+            //Search for pyramid location
+            for (int i = (int)(Main.maxTilesX * 0.1f); i < Main.maxTilesY * 0.9f; i++) {
+                progress.Set(i / (Main.maxTilesY * 0.9f));
+                for (int j = (int)(Main.worldSurface * 0.5); j < Main.worldSurface * 1.25f; j++) {
+                    Dictionary<ushort, int> tileData = new Dictionary<ushort, int>();
+                    WorldUtils.Gen(new Point(i, j), new Shapes.Rectangle(51, 51), new Actions.TileScanner(TileID.Sand).Output(tileData));
+
+                    //If this area contains 500 blocks of sand, it is a valid spot; generate here
+                    if (tileData[TileID.Sand] > 500) {
+                        StructureData pyramidStructure = IOUtils.GetStructureFromFile(LivingWorldMod.LWMStructurePath + "Dungeons/OverworldPyramid.struct");
+
+                        WorldGenUtils.GenerateStructure(pyramidStructure, i, j);
+                        return;
+                    }
+                }
+            }
+
+            //If we got this far, then nothing valid was found; drop warning in console
+            ModContent.GetInstance<LivingWorldMod>().Logger.Warn("Revamped Pyramid unable to generate due to no valid placement.");
         }
     }
 }
