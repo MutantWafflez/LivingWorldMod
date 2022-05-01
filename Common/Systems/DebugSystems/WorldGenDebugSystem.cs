774946c46bafc9728900863d6565ac8d22d77551
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2022-04-30 23:02:29 -0700

    Move Sublib back to dedicated branch

--- Common/Systems/DebugSystems/WorldGenDebugSystem.cs
+++ Common/Systems/DebugSystems/WorldGenDebugSystem.cs
@@ -26,47 +26,6 @@ namespace LivingWorldMod.Common.Systems.DebugSystems {
             Dust.QuickBox(new Vector2(x, y) * 16, new Vector2(x + 2, y + 3) * 16, 2, Color.YellowGreen, null);
 
             // Code to test placed here:
-
-            Point tipOfPyramid = new Point(x, y);
-            int lengthOfPyramid = 271;
-
-            ShapeData fullPyramidData = new ShapeData();
-
-            //Generate outer-shell of the pyramid, solid in the beginning
-            WorldUtils.Gen(tipOfPyramid, new EqualTriangle(lengthOfPyramid), Actions.Chain(
-                new Actions.SetTile(TileID.SandStoneSlab, true),
-                new Actions.PlaceWall((ushort)ModContent.WallType<PyramidBrickWall>()),
-                new Actions.Blank().Output(fullPyramidData)
-            ));
-
-            //Remove walls on outer-most layer
-            WorldUtils.Gen(tipOfPyramid, new ModShapes.All(fullPyramidData), Actions.Chain(
-                new Modifiers.IsTouchingAir(),
-                new Actions.RemoveWall()
-            ));
-
-            ShapeData alteredPyramidData = new ShapeData(fullPyramidData);
-
-            //Generate entrance hole
-            WorldUtils.Gen(tipOfPyramid + new Point(-15, (int)(lengthOfPyramid * 0.025f)), new Shapes.Rectangle(20, 9), Actions.Chain(
-                new Actions.ClearTile(true),
-                new Actions.Blank().Output(alteredPyramidData)
-            ));
-
-            //Generated cracked bricks below entrance
-            WorldUtils.Gen(tipOfPyramid + new Point(-5, 15), new Shapes.Rectangle(10, 5), Actions.Chain(
-                new Actions.SetTileKeepWall((ushort)ModContent.TileType<CrackedSandstoneSlab>(), true),
-                new Actions.Blank().Output(alteredPyramidData)
-            ));
-
-            int firstRoomRadius = 15;
-            Point firstRoomOrigin = tipOfPyramid + new Point(0, 20 + firstRoomRadius);
-
-            //Generate static, initial puzzle room
-            WorldUtils.Gen(firstRoomOrigin, new Shapes.Slime(firstRoomRadius), Actions.Chain(
-                new Actions.ClearTile(true),
-                new Actions.Blank().Output(alteredPyramidData)
-            ));
         }
     }
 }
