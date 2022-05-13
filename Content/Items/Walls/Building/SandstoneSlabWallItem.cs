be7bf1d2e919b7cfe537ac68d4e5ca0359f24809
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2022-05-12 23:25:22 -0700

    Add Sandstone Slab Wall

---
+++ Content/Items/Walls/Building/SandstoneSlabWallItem.cs
@@ -0,0 +1,12 @@
+﻿using LivingWorldMod.Content.Walls.Building;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace LivingWorldMod.Content.Items.Walls.Building {
+    public class SandstoneSlabWallItem : BaseItem {
+        public override void SetDefaults() {
+            Item.CloneDefaults(ItemID.StoneSlabWall);
+            Item.createWall = ModContent.WallType<SandstoneSlabWall>();
+        }
+    }
+}
---
+++ Content/Walls/Building/SandstoneSlabWall.cs
@@ -0,0 +1,20 @@
+﻿using LivingWorldMod.Content.Items.Walls.Building;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace LivingWorldMod.Content.Walls.Building {
+    public class SandstoneSlabWall : BaseWall {
+        public override void SetStaticDefaults() {
+            Main.wallHouse[Type] = true;
+            Main.wallLargeFrames[Type] = Main.wallLargeFrames[WallID.StoneSlab];
+
+            DustType = DustID.Sand;
+
+            ItemDrop = ModContent.ItemType<SandstoneSlabWallItem>();
+
+            AddMapEntry(new Color(108, 103, 72));
+        }
+    }
+}
