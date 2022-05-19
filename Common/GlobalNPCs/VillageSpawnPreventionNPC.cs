8271600817623745c445ffc5d567bb5c536de49a
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2022-05-19 15:43:55 -0700

    Prevent enemy spawning in village zones

---
+++ Common/GlobalNPCs/VillageSpawnPreventionNPC.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LivingWorldMod.Common.Systems;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace LivingWorldMod.Common.GlobalNPCs {
+    /// <summary>
+    /// Simple GlobalNPC that prevents spawning if the player is within a village.
+    /// </summary>
+    public class VillageSpawnPreventionNPC : GlobalNPC {
+        public override void EditSpawnPool(IDictionary<int, float> pool, NPCSpawnInfo spawnInfo) {
+            WorldCreationSystem creationSystem = ModContent.GetInstance<WorldCreationSystem>();
+
+            if (creationSystem.villageZones.Any(zone => zone.Contains(spawnInfo.Player.Center.ToTileCoordinates()))) {
+                pool[0] = 0;
+            }
+        }
+    }
+}
