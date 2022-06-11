a35c1620a2fff216999c8b9578716d8200813e00
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2022-06-10 19:09:27 -0700

    Fried Harpy Egg (for the meme)

---
+++ Content/Items/Food/FriedHarpyEgg.cs
@@ -0,0 +1,46 @@
+﻿using LivingWorldMod.Content.Items.Miscellaneous;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+using Terraria.ID;
+
+namespace LivingWorldMod.Content.Items.Food {
+    public class FriedHarpyEgg : BaseItem {
+        public override void SetDefaults() {
+            Item.CloneDefaults(ItemID.FriedEgg);
+
+            Item.buffType = BuffID.WellFed3;
+            Item.buffTime = 14400; //4 minutes
+            Item.rare = ItemRarityID.Blue;
+
+            ItemID.Sets.IsFood[Type] = true;
+            ItemID.Sets.FoodParticleColors[Type] = ItemID.Sets.FoodParticleColors[ItemID.FriedEgg];
+        }
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale) {
+            //Food item drawing is a bit wack
+            Rectangle rectanglizedPosition = new Rectangle((int)position.X - 6, (int)position.Y + 8, 28, 16);
+            Rectangle newFrame = new Rectangle(0, 0, 28, 16);
+
+            spriteBatch.Draw(TextureAssets.Item[Type].Value, rectanglizedPosition, newFrame, drawColor, 0f, Vector2.Zero, SpriteEffects.None, scale);
+
+            return false;
+        }
+
+        public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI) {
+            Rectangle rectanglizedPosition = new Rectangle((int)Item.position.X, (int)Item.position.Y, 28, 16);
+            Rectangle frame = new Rectangle(0, 0, 28, 16);
+
+            spriteBatch.Draw(TextureAssets.Item[Type].Value, rectanglizedPosition, frame, alphaColor, rotation, Vector2.Zero, SpriteEffects.None, scale);
+
+            return false;
+        }
+
+        public override void AddRecipes() {
+            CreateRecipe()
+                .AddIngredient<HarpyEgg>()
+                .AddTile(TileID.CookingPots)
+                .Register();
+        }
+    }
+}
---
+++ Content/Items/Miscellaneous/HarpyEgg.cs
@@ -0,0 +1,14 @@
+﻿using Terraria.ID;
+
+namespace LivingWorldMod.Content.Items.Miscellaneous {
+    /// <summary>
+    /// Item used for respawning Harpy Villagers at the Harpy Village shrine.
+    /// </summary>
+    public class HarpyEgg : BaseItem {
+        public override void SetDefaults() {
+            Item.width = 22;
+            Item.height = 24;
+            Item.rare = ItemRarityID.Blue;
+        }
+    }
+}
