296b0cdf5080329b5ce9962672442f2f945388e9
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2022-06-13 00:47:55 -0700

    Add back code that got lost in re-implementation

--- Core/Patches/NPCHousingPatches.cs
+++ Core/Patches/NPCHousingPatches.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using LivingWorldMod.Common.Systems;
+using LivingWorldMod.Common.Systems.UI;
 using LivingWorldMod.Content.NPCs.Villagers;
 using LivingWorldMod.Content.TileEntities.Interactables;
 using LivingWorldMod.Custom.Enums;
@@ -153,8 +154,7 @@ namespace LivingWorldMod.Core.Patches {
 
                 //Re-add our check, making sure it's inverted compared to the IL, since in IL it determines if the code should run if these values are FALSE,
                 // but since we took control of the instructions, we can test based on if it's true or not for easy understanding
-                //TODO: Swap back to commented expression when reputation system is re-implemented
-                c.EmitDelegate<Func<bool>>(() => Main.EquipPage == 1 /*|| ModContent.GetInstance<VillagerHousingUISystem>().housingState.isMenuVisible*/);
+                c.EmitDelegate<Func<bool>>(() => Main.EquipPage == 1 || VillagerHousingUISystem.Instance.correspondingUIState.isMenuVisible);
 
                 c.Emit(OpCodes.Brtrue_S, stolenLabel);
             }
