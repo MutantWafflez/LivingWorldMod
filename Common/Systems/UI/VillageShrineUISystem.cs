76689513b7a42aa18c6315c68b9ab7003270e273
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2022-06-13 02:38:03 -0700

    Village Shrine UI Beginnings

---
+++ Common/Systems/UI/VillageShrineUISystem.cs
@@ -0,0 +1,13 @@
+﻿using LivingWorldMod.Common.Systems.BaseSystems;
+using LivingWorldMod.Content.UI.VillageShrine;
+using Terraria.UI;
+
+namespace LivingWorldMod.Common.Systems.UI {
+    public class VillageShrineUISystem : UISystem<VillageShrineUIState> {
+        public override string VanillaInterfaceLocation => "Vanilla: Inventory";
+
+        public override string InternalInterfaceName => "Village Shrine Panel";
+
+        public override InterfaceScaleType ScaleType => InterfaceScaleType.None;
+    }
+}
---
+++ Content/UI/VillageShrine/VillageShrineUIState.cs
@@ -0,0 +1,8 @@
+﻿using Terraria.UI;
+
+namespace LivingWorldMod.Content.UI.VillageShrine {
+    /// <summary>
+    /// UIState that handles the UI for the village shrine for each type of village.
+    /// </summary>
+    public class VillageShrineUIState : UIState { }
+}
