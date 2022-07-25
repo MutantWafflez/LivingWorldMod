2af3687fe6a1a4457fde3d43987f05bb2180f48e
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2022-07-24 19:31:44 -0700

    NPC rain strolling beginnings

---
+++ Common/GlobalNPCs/TownProfileNPC.cs
@@ -0,0 +1,22 @@
+﻿using LivingWorldMod.Common.VanillaOverrides.NPCProfiles;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace LivingWorldMod.Common.GlobalNPCs {
+    /// <summary>
+    /// GlobalNPC that handles visual updates on town NPCs for various aesthetic purposes.
+    /// </summary>
+    [Autoload(Side = ModSide.Client)]
+    public class TownProfileNPC : GlobalNPC {
+        public override ITownNPCProfile ModifyTownNPCProfile(NPC npc) {
+            //Rain profiles
+            if (npc.type == NPCID.Guide) {
+                return new GuideProfile();
+            }
+
+            return null;
+        }
+    }
+}
