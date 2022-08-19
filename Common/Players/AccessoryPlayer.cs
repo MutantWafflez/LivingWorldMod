0202f8fdfbb63a9ccc6305b5958adea8a2d115ea
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2022-08-18 18:29:06 -0700

    Accessory Handling system begginnings

---
+++ Common/Players/AccessoryPlayer.cs
@@ -0,0 +1,8 @@
+﻿using Terraria.ModLoader;
+
+namespace LivingWorldMod.Common.Players {
+    /// <summary>
+    /// ModPlayer that handles the accessories in this mod.
+    /// </summary>
+    public class AccessoryPlayer : ModPlayer { }
+}
---
+++ Content/Items/Accessories/AccessoryItem.cs
@@ -0,0 +1,10 @@
+﻿using LivingWorldMod.Common.Players;
+
+namespace LivingWorldMod.Content.Items.Accessories {
+    /// <summary>
+    /// ModItem class for specifically accessories that works in direct tandem with <seealso cref="AccessoryPlayer"/>.
+    /// </summary>
+    public abstract class AccessoryItem : BaseItem {
+        public override void SetStaticDefaults() { }
+    }
+}
