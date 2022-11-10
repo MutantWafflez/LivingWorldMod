be36d9c8dd0320f2186174dabb1a20d359376812
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2022-11-09 19:01:26 -0800

    Curse of Adhesion

--- Core/Patches/PlayerUpdatePatches.cs
+++ Core/Patches/PlayerUpdatePatches.cs
@@ -3,6 +3,7 @@ using LivingWorldMod.Common.Players;
 using LivingWorldMod.Content.Subworlds.Pyramid;
 using LivingWorldMod.Core.PacketHandlers;
 using LivingWorldMod.Custom.Utilities;
+using Microsoft.Xna.Framework;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using Terraria;
@@ -18,6 +19,7 @@ namespace LivingWorldMod.Core.Patches {
         public void Load(Mod mod) {
             On.Terraria.Player.JumpMovement += PlayerGravityManipulation;
             IL.Terraria.Player.Update += ForceWaterPhysics;
+            IL.Terraria.Player.StickyMovement += ForceStickyPhysics;
         }
 
         public void Unload() { }
@@ -60,6 +62,25 @@ namespace LivingWorldMod.Core.Patches {
             //Add our own check
             c.Emit(OpCodes.Ldarg_0);
             c.EmitDelegate<Func<bool, Player, bool>>((originalValue, player) => player.GetModPlayer<PyramidDungeonPlayer>().CurrentCurses.Contains(PyramidRoomCurseType.Viscosity) || originalValue);
+        }
+
+        private void ForceStickyPhysics(ILContext il) {
+            //Small edit that will force the physical effects of touching a honey block, regardless of whether or not
+            //the player is actually touching a honey block. Player must be colliding with a block for this to apply.
+
+            ILCursor c = new ILCursor(il);
+
+            //Navigate to Sticky Tile call.
+            c.ErrorOnFailedGotoNext(MoveType.After, i => i.MatchCall<Collision>(nameof(Collision.StickyTiles)));
+            c.Emit(OpCodes.Pop);
+            c.Emit(OpCodes.Ldarg_0);
+            c.EmitDelegate<Func<Player, Vector2>>(player => {
+                if (player.GetModPlayer<PyramidDungeonPlayer>().CurrentCurses.Contains(PyramidRoomCurseType.Adhesion)) {
+                    return Collision.IsClearSpotTest(player.position + player.velocity, 1f, player.width, player.height, gravDir: (int)player.gravDir, checkSlopes: true) ? new Vector2(-1f) : new Vector2(0f);
+                }
+
+                return Collision.StickyTiles(player.position, player.velocity, player.width, player.height);
+            });
         }
     }
 }
