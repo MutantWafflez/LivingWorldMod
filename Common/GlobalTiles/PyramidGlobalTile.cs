aadc08099d0a010bb05a416111f7e1e1c9654920
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2022-11-12 11:44:56 -0800

    Preventing destruction in subworld

---
+++ Common/GlobalTiles/PyramidGlobalTile.cs
@@ -0,0 +1,21 @@
+﻿using LivingWorldMod.Common.Players;
+using Terraria;
+using LivingWorldMod.Content.Subworlds.Pyramid;
+using Microsoft.Xna.Framework.Graphics;
+using SubworldLibrary;
+using Terraria.ModLoader;
+
+namespace LivingWorldMod.Common.GlobalTiles {
+    /// <summary>
+    /// Global Tile used exclusively in the Pyramid Dungeon.
+    /// </summary>
+    public class PyramidGlobalTile : GlobalTile {
+        public bool IsInPyramidSubworld => SubworldSystem.IsActive<PyramidSubworld>();
+
+        public override bool CanKillTile(int i, int j, int type, ref bool blockDamaged) => !IsInPyramidSubworld || LivingWorldMod.IsDebug;
+
+        public override bool CanExplode(int i, int j, int type) => !IsInPyramidSubworld || LivingWorldMod.IsDebug;
+
+        public override bool PreDraw(int i, int j, int type, SpriteBatch spriteBatch) => Main.LocalPlayer.GetModPlayer<PyramidDungeonPlayer>().currentRoom?.region.Contains(i, j) ?? true;
+    }
+}
---
+++ Common/GlobalWalls/PyramidGlobalWall.cs
@@ -0,0 +1,23 @@
+﻿using LivingWorldMod.Common.Players;
+using LivingWorldMod.Content.Subworlds.Pyramid;
+using Microsoft.Xna.Framework.Graphics;
+using SubworldLibrary;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace LivingWorldMod.Common.GlobalWalls {
+    /// <summary>
+    /// Global Wall used exclusively in the Pyramid Dungeon.
+    /// </summary>
+    public class PyramidGlobalWall : GlobalWall {
+        public bool IsInPyramidSubworld => SubworldSystem.IsActive<PyramidSubworld>();
+
+        public override void KillWall(int i, int j, int type, ref bool fail) {
+            fail = IsInPyramidSubworld && !LivingWorldMod.IsDebug;
+        }
+
+        public override bool CanExplode(int i, int j, int type) => !IsInPyramidSubworld || LivingWorldMod.IsDebug;
+
+        public override bool PreDraw(int i, int j, int type, SpriteBatch spriteBatch) => Main.LocalPlayer.GetModPlayer<PyramidDungeonPlayer>().currentRoom?.region.Contains(i, j) ?? true;
+    }
+}
