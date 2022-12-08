8f3218a238bbf3ac3eb02c0b09d23331ad444bc4
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2022-12-07 22:41:37 -0800

    Purge Accessory system, time for a rework!

---
+++ Core/Patches/ItemSlotPatches.cs
@@ -0,0 +1,19 @@
+﻿using On.Terraria.UI;
+using Terraria.ModLoader;
+
+namespace LivingWorldMod.Core.Patches {
+    public class ItemSlotPatches : ILoadable {
+        public void Load(Mod mod) {
+            ItemSlot.isEquipLocked += ExpandedEquipLock;
+        }
+
+        public void Unload() { }
+
+        private bool ExpandedEquipLock(ItemSlot.orig_isEquipLocked orig, int type) {
+            //"Revamped" isEquipLocked for additional and adaptable functionality.
+            bool origValue = orig(type);
+
+            return origValue;
+        }
+    }
+}
--- Custom/Utilities/PlayerUtils.cs
+++ Custom/Utilities/PlayerUtils.cs
@@ -1,22 +1,10 @@
-﻿using LivingWorldMod.Common.Players;
-using LivingWorldMod.Content.Items.Accessories;
-using Terraria;
-using Terraria.DataStructures;
+﻿using Terraria;
 
 namespace LivingWorldMod.Custom.Utilities {
     /// <summary>
     /// Utilities class that holds methods that pertains to players.
     /// </summary>
     public static class PlayerUtils {
-        /// <summary>
-        /// Whether or not in the Player.KillMe method, tML's PreKill should
-        /// be skipped.
-        /// </summary>
-        public static bool SkipPreKill {
-            get;
-            private set;
-        }
-
         /// <summary>
         /// Calculates and returns the entirety of the savings of the player in all applicable inventories.
         /// </summary>
@@ -32,25 +20,6 @@ namespace LivingWorldMod.Custom.Utilities {
             long voidVaultCashCount = Utils.CoinsCount(out _, player.bank4.item);
 
             return Utils.CoinsCombineStacks(out _, playerInvCashCount, piggyCashCount, safeCashCount, defForgeCashCount, voidVaultCashCount);
-        }
-
-        /// <summary>
-        /// A wrapper method for the Player.KillMe method that respects LivingWorldMod's
-        /// <seealso cref="AccessoryItem.PrePlayerForceKill"/> method.
-        /// </summary>
-        public static void AttemptForceKill(this Player player, PlayerDeathReason deathReason) {
-            bool shouldKill = true;
-            foreach (AccessoryItem accessory in player.GetModPlayer<AccessoryPlayer>().ActiveAccessoryItems) {
-                if (!accessory.PrePlayerForceKill(player, deathReason)) {
-                    shouldKill = false;
-                }
-            }
-
-            if (shouldKill) {
-                SkipPreKill = true;
-                player.KillMe(deathReason, player.statLife, 0);
-                SkipPreKill = false;
-            }
         }
     }
 }
