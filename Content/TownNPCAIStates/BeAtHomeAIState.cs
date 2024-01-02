a6099ccc69e9ab262b948c66a3b83c7934b6d16e
MutantWafflez <61396445+MutantWafflez@users.noreply.github.com>
2024-01-02 05:56:46 -0800

    Update sleeping & housing code

---
+++ Content/TownNPCAIStates/BeAtHomeAIState.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using LivingWorldMod.Common.GlobalNPCs;
+using LivingWorldMod.Common.ModTypes;
+using LivingWorldMod.Custom.Classes.TownNPCModules;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+
+namespace LivingWorldMod.Content.TownNPCAIStates;
+
+public sealed class BeAtHomeAIState : TownNPCAIState {
+    public override int ReservedStateInteger => 27;
+
+    public override void DoState(TownGlobalNPC globalNPC, NPC npc) {
+        if (!TownNPCHousingModule.ShouldGoHome) {
+            TownGlobalNPC.RefreshToState<DefaultAIState>(npc);
+            return;
+        }
+
+        TownNPCPathfinderModule pathfinderModule = globalNPC.PathfinderModule;
+        Point restPos = globalNPC.HousingModule.RestPos;
+        if (pathfinderModule.BottomLeftTileOfNPC != restPos) {
+            npc.ai[1] = 0f;
+            pathfinderModule.RequestPathfind(restPos, null);
+        }
+        else if (TownNPCHousingModule.ShouldSleep) {
+            Tile restTile = Main.tile[restPos];
+            npc.BottomLeft = restPos.ToWorldCoordinates(0f, 16f);
+            if (TileID.Sets.CanBeSleptIn[restTile.TileType]) {
+                npc.friendlyRegen += 20;
+
+                PlayerSleepingHelper.GetSleepingTargetInfo(restPos.X, restPos.Y, out int targetDirection, out _, out _);
+                npc.direction = targetDirection;
+                npc.rotation = MathHelper.PiOver2 * -targetDirection;
+                Main.sleepingManager.AddNPC(npc.whoAmI, restPos);
+                globalNPC.SpriteModule.RequestBlink();
+
+                npc.ai[1] = 1f;
+                // TODO: Add mood boost for good rest
+                npc.ai[2] += 2f;
+            }
+            else if (TileID.Sets.CanBeSatOnForNPCs[restTile.TileType]) {
+                npc.friendlyRegen += 15;
+
+                npc.SitDown(restPos, out int direction, out _);
+                npc.direction = direction;
+                Main.sittingManager.AddNPC(npc.whoAmI, restPos);
+                globalNPC.SpriteModule.RequestBlink();
+
+                npc.ai[1] = 1f;
+                npc.ai[2] += 1f;
+            }
+        }
+    }
+
+    public override void FrameNPC(TownGlobalNPC globalNPC, NPC npc, int frameHeight) {
+        Tile restTile = Main.tile[globalNPC.HousingModule.RestPos];
+        // Set NPC to sitting frame
+        if (npc.ai[1] == 1f && TileID.Sets.CanBeSatOnForNPCs[restTile.TileType]) {
+            npc.frame.Y = frameHeight * (Main.npcFrameCount[npc.type] - NPCID.Sets.AttackFrameCount[npc.type] - 3);
+        }
+    }
+
+    public override void PostDrawNPC(TownGlobalNPC globalNPC, NPC npc, SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor) {
+        if (npc.ai[1] != 1f) {
+            return;
+        }
+
+        Main.instance.LoadItem(ItemID.SleepingIcon);
+        Texture2D sleepingIconTexture = TextureAssets.Item[ItemID.SleepingIcon].Value;
+        spriteBatch.Draw(
+            sleepingIconTexture,
+            npc.Top - screenPos + new Vector2(sleepingIconTexture.Width / -2f, -16 + MathF.Sin(Main.GlobalTimeWrappedHourly)),
+            drawColor * 0.67f
+        );
+    }
+}
--- Custom/Classes/TownNPCModules/TownNPCHousingModule.cs
+++ Custom/Classes/TownNPCModules/TownNPCHousingModule.cs
@@ -14,7 +14,7 @@ public class TownNPCHousingModule : TownNPCModule {
         private set;
     }
 
-    public Point? RestPos {
+    public Point RestPos {
         get;
         private set;
     }
@@ -29,8 +29,7 @@ public class TownNPCHousingModule : TownNPCModule {
 
     public override void Update() {
         if (npc.homeTileX == -1 && npc.homeTileY == -1 && npc.velocity.Y == 0f && !npc.shimmering) {
-            //npc.UpdateHomeTileState(npc.homeless, (int)npc.Center.X / 16, (int)(npc.position.Y + npc.height + 4f) / 16);
-            npc.UpdateHomeTileState(npc.homeless, Main.spawnTileX, Main.spawnTileY);
+            npc.UpdateHomeTileState(npc.homeless, (int)npc.Center.X / 16, (int)(npc.position.Y + npc.height + 4f) / 16);
         }
 
         HomelessTeleportCheck();
@@ -56,85 +55,8 @@ public class TownNPCHousingModule : TownNPCModule {
         Utils.DrawRect(spriteBatch, worldBoundingBox, Main.DiscoColor);
     }
 
-    private void HomelessTeleportCheck() {
-        //Adapted vanilla code
-        Point bottomOfNPC = (npc.Bottom + new Vector2(0, 1f)).ToTileCoordinates();
-        FindRestingSpot(out int floorX, out int floorY);
-        RestPos = /*floorX == npc.homeTileX && floorY == npc.homeTileY ? null :*/ new Point(floorX, floorY - 1);
-
-        if (!WorldGen.InWorld(bottomOfNPC.X, bottomOfNPC.Y) || Main.netMode == NetmodeID.MultiplayerClient && !Main.sectionManager.TileLoaded(bottomOfNPC.X, bottomOfNPC.Y)) {
-            return;
-        }
-
-        if (Main.netMode == NetmodeID.MultiplayerClient) {
-            return;
-        }
-
-        TownNPCPathfinderModule pathfinderModule = GlobalNPC.PathfinderModule;
-        if (!ShouldGoHome || pathfinderModule.IsPathfinding) {
-            return;
-        }
-
-        int goHomeState = TownNPCAIState.GetStateInteger<GoHomeAIState>();
-        if (npc.ai[0] != goHomeState) {
-            TownGlobalNPC.RefreshToState(npc, goHomeState);
-        }
-
-        bool nearbyPlayers = false;
-        for (int i = 0; i < 2; i++) {
-            Rectangle playerZoneCheck = i == 1
-                ? new Rectangle(
-                    (int)(npc.position.X + npc.width / 2f - NPC.sWidth / 2f - NPC.safeRangeX),
-                    (int)(npc.position.Y + npc.height / 2f - NPC.sHeight / 2f - NPC.safeRangeY),
-                    NPC.sWidth + NPC.safeRangeX * 2,
-                    NPC.sHeight + NPC.safeRangeY * 2
-                )
-                : new Rectangle(
-                    npc.homeTileX * 16 + 8 - NPC.sWidth / 2 - NPC.safeRangeX,
-                    npc.homeTileY * 16 + 8 - NPC.sHeight / 2 - NPC.safeRangeY,
-                    NPC.sWidth + NPC.safeRangeX * 2,
-                    NPC.sHeight + NPC.safeRangeY * 2
-                );
-
-            for (int j = 0; j < Main.maxPlayers; j++) {
-                if (!Main.player[j].active || !new Rectangle((int)Main.player[j].position.X, (int)Main.player[j].position.Y, Main.player[j].width, Main.player[j].height).Intersects(playerZoneCheck)) {
-                    continue;
-                }
-
-                nearbyPlayers = true;
-                goto CheckTeleport_Attempt;
-            }
-        }
-
-        CheckTeleport_Attempt:
-        if (nearbyPlayers) {
-            return;
-        }
-
-        bool validHouse = false;
-        for (int i = -1; i < 2; i++) {
-            int tileXByOffset = floorX + i;
-            if (Collision.SolidTiles(tileXByOffset - 1, tileXByOffset + 1, floorY - 3, floorY - 1)) {
-                continue;
-            }
-
-            pathfinderModule.CancelPathfind();
-            npc.velocity = Vector2.Zero;
-            npc.BottomLeft = new Vector2(floorX, floorY).ToWorldCoordinates(Vector2.Zero);
-            npc.netUpdate = validHouse = true;
-            break;
-        }
-
-        if (validHouse) {
-            return;
-        }
-
-        npc.homeless = true;
-        WorldGen.QuickFindHome(npc.whoAmI);
-    }
-
     // Adapted vanilla code
-    private void FindRestingSpot(out int floorX, out int floorY) {
+    public void FindRestingSpot(out int floorX, out int floorY) {
         floorX = npc.homeTileX;
         floorY = npc.homeTileY;
         if (floorX == -1 || floorY == -1 || RoomBoundingBox is not { } boundingBox) {
@@ -184,38 +106,81 @@ public class TownNPCHousingModule : TownNPCModule {
         }
         finalRestPos.Y++;
 
-        /*
-        switch (restTile.TileType) {
-            case TileID.Chairs or TileID.Toilets: {
-                finalRestPos = TileUtils.GetCornerOfMultiTile(restTile, finalRestPos.X, finalRestPos.Y, TileUtils.CornerType.BottomLeft);
-                finalRestPos.Y++;
-                break;
-            }
-            default: {
-                if (restTile.TileType == TileID.Beds || restTile.TileType >= TileID.Count) {
-                    TileRestingInfo info = new(npc, finalRestPos, Vector2.Zero, npc.direction);
-                    if (TileID.Sets.CanBeSleptIn[restTile.TileType]) {
-                        PlayerSleepingHelper.GetSleepingTargetInfo(finalRestPos.X, finalRestPos.Y, out int targetDirection, out Vector2 anchorPos, out _);
-                    }
-                    else {
-                        TileLoader.ModifySittingTargetInfo(finalRestPos.X, finalRestPos.Y, restTile.TileType, ref info);
-                    }
-
-                    finalRestPos = info.AnchorTilePosition;
-                    finalRestPos.Y++;
-                }
-                break;
-            }
-        }*/
-
-        /*
-        for (int j = 0; j < 200; j++) {
-            if (Main.npc[j].active && Main.npc[j].aiStyle == 7 && Main.npc[j].townNPC && Main.npc[j].ai[0] == 5f && (Main.npc[j].Bottom + Vector2.UnitY * -2f).ToTileCoordinates() == finalRestPos) {
-                return;
-            }
-        }*/
-
         floorX = finalRestPos.X;
         floorY = finalRestPos.Y;
+    }
+
+
+    private void HomelessTeleportCheck() {
+        //Adapted vanilla code
+        Point bottomOfNPC = (npc.Bottom + new Vector2(0, 1f)).ToTileCoordinates();
+        FindRestingSpot(out int floorX, out int floorY);
+        RestPos = new Point(floorX, floorY - 1);
+
+        if (!WorldGen.InWorld(bottomOfNPC.X, bottomOfNPC.Y) || Main.netMode == NetmodeID.MultiplayerClient && !Main.sectionManager.TileLoaded(bottomOfNPC.X, bottomOfNPC.Y)) {
+            return;
+        }
+
+        if (Main.netMode == NetmodeID.MultiplayerClient || !ShouldGoHome) {
+            return;
+        }
+
+        TownNPCPathfinderModule pathfinderModule = GlobalNPC.PathfinderModule;
+        int beAtHomeStateInt = TownNPCAIState.GetStateInteger<BeAtHomeAIState>();
+        if (npc.ai[0] != beAtHomeStateInt) {
+            TownGlobalNPC.RefreshToState(npc, beAtHomeStateInt);
+            pathfinderModule.CancelPathfind();
+        }
+
+        bool nearbyPlayers = false;
+        for (int i = 0; i < 2; i++) {
+            Rectangle playerZoneCheck = i == 1
+                ? new Rectangle(
+                    (int)(npc.Center.X - NPC.sWidth / 2f - NPC.safeRangeX),
+                    (int)(npc.Center.Y + npc.height / 2f - NPC.sHeight / 2f - NPC.safeRangeY),
+                    NPC.sWidth + NPC.safeRangeX * 2,
+                    NPC.sHeight + NPC.safeRangeY * 2
+                )
+                : new Rectangle(
+                    npc.homeTileX * 16 + 8 - NPC.sWidth / 2 - NPC.safeRangeX,
+                    npc.homeTileY * 16 + 8 - NPC.sHeight / 2 - NPC.safeRangeY,
+                    NPC.sWidth + NPC.safeRangeX * 2,
+                    NPC.sHeight + NPC.safeRangeY * 2
+                );
+
+            for (int j = 0; j < Main.maxPlayers; j++) {
+                if (!Main.player[j].active || !new Rectangle((int)Main.player[j].position.X, (int)Main.player[j].position.Y, Main.player[j].width, Main.player[j].height).Intersects(playerZoneCheck)) {
+                    continue;
+                }
+
+                nearbyPlayers = true;
+                goto CheckTeleport_Attempt;
+            }
+        }
+
+        CheckTeleport_Attempt:
+        if (nearbyPlayers) {
+            return;
+        }
+
+        bool validHouse = false;
+        for (int i = -1; i < 2; i++) {
+            int tileXByOffset = floorX + i;
+            if (Collision.SolidTiles(tileXByOffset - 1, tileXByOffset + 1, floorY - 3, floorY - 1)) {
+                continue;
+            }
+
+            npc.velocity = Vector2.Zero;
+            npc.BottomLeft = new Vector2(floorX, floorY).ToWorldCoordinates(Vector2.Zero);
+            npc.netUpdate = validHouse = true;
+            break;
+        }
+
+        if (validHouse) {
+            return;
+        }
+
+        npc.homeless = true;
+        WorldGen.QuickFindHome(npc.whoAmI);
     }
 }
