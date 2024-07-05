7c2d05a33536fbc14fc6a2820540ea94812a5872
MutantWafflez <61396445+mutantwafflez@users.noreply.github.com>
2024-07-04 21:21:09 -0700

    "Finish" event preference structure

--- Content/TownNPCRevitalization/DataStructures/Classes/ShopPersonalityTraits/EventPersonalityTrait.cs
+++ Content/TownNPCRevitalization/DataStructures/Classes/ShopPersonalityTraits/EventPersonalityTrait.cs
@@ -6,18 +6,23 @@ using Terraria.GameContent.Personalities;
 
 namespace LivingWorldMod.Content.TownNPCRevitalization.DataStructures.Classes.ShopPersonalityTraits;
 
+/// <summary>
+/// Similar to biome/NPC preferences for Town NPCs happiness, except for "events" which are denoted active by some arbitrary function returning a bool. 
+/// </summary>
 public class EventPersonalityTrait(params EventPersonalityTrait.EventPreference[] preferences) : IShopPersonalityTrait, IEnumerable<EventPersonalityTrait.EventPreference> {
-    public record struct EventPreference(AffectionLevel AffectionLevel, Func<bool> EventActiveFunction);
+    public record struct EventPreference(AffectionLevel AffectionLevel, Func<bool> EventActiveFunction, string EventName);
 
     private readonly List<EventPreference> _preferences = [..preferences];
 
     public void ModifyShopPrice(HelperInfo info, ShopHelper shopHelperInstance) {
-        foreach ((AffectionLevel affectionLevel, Func<bool> eventActiveFunction) in _preferences) {
-            
+        foreach ((AffectionLevel affectionLevel, Func<bool> eventActiveFunction, string eventName) in _preferences) {
+            if (eventActiveFunction()) {
+                shopHelperInstance.AddHappinessReportText($"{affectionLevel}Event_{eventName}");
+            }
         }
     }
 
-    public IEnumerator<EventPreference> GetEnumerator() => throw new NotImplementedException();
+    public IEnumerator<EventPreference> GetEnumerator() => _preferences.GetEnumerator();
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
