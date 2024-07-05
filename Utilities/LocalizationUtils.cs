00df995872e543c53af3757425a8b030f9bb1374
MutantWafflez <61396445+mutantwafflez@users.noreply.github.com>
2024-07-05 16:40:56 -0700

    Add PrependModKey extension

--- Utilities/LocalizationUtils.cs
+++ Utilities/LocalizationUtils.cs
@@ -6,8 +6,13 @@ namespace LivingWorldMod.Utilities;
 public static partial class LWMUtils {
     /// <summary>
     ///     Extension for strings that will turn the provided string into its <see cref="LocalizedText" /> equivalent, assuming
-    ///     the
-    ///     provided string is the key for a <see cref="LWM" /> localization file text value.
+    ///     the provided string is the key for a <see cref="LWM" /> localization file text value.
     /// </summary>
     public static LocalizedText Localized(this string key) => ModContent.GetInstance<LWM>().GetLocalization(key);
+
+    /// <summary>
+    ///     Extension for strings that will prepend "Mods.LivingWorldMod." to the string, for use with generating localization keys
+    ///     instead of <see cref="LocalizedText" /> objects directly.
+    /// </summary>
+    public static string PrependModKey(this string suffix) => $"Mods.{nameof(LivingWorldMod)}.{suffix}";
 }
