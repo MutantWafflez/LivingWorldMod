42e4c284c87aa8e44c4f55e2ea340767c7fbeddb
MutantWafflez <61396445+mutantwafflez@users.noreply.github.com>
2024-07-17 03:46:21 -0700

    Move to COMPLETE hijack of vanilla happiness system

--- Content/TownNPCRevitalization/Globals/Patches/HappinessPatches.cs
+++ Content/TownNPCRevitalization/Globals/Patches/HappinessPatches.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using LivingWorldMod.Content.TownNPCRevitalization.DataStructures.Classes.TownNPCModules;
 using LivingWorldMod.Content.TownNPCRevitalization.Globals.NPCs;
 using LivingWorldMod.DataStructures.Classes;
-using LivingWorldMod.Utilities;
 using Microsoft.Xna.Framework;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using Terraria.GameContent;
+using Terraria.GameContent.Personalities;
 
 namespace LivingWorldMod.Content.TownNPCRevitalization.Globals.Patches;
 
@@ -22,37 +23,50 @@ public sealed class HappinessPatches : LoadablePatch {
 
         ILCursor c = new(il);
 
-        // Jump AFTER vanilla _currentHappiness set, so our "Content" Mood modifier is properly ignored
-        c.GotoNext(MoveType.After, i => i.Match(OpCodes.Stfld));
         c.Emit(OpCodes.Ldarg_0);
+        c.Emit(OpCodes.Ldarg_1);
         c.Emit(OpCodes.Ldarg_2);
-        c.EmitDelegate<Action<ShopHelper, NPC>>(
-            (shopHelper, npc) => {
-                if (!npc.TryGetGlobalNPC(out TownGlobalNPC globalNPC)) {
-                    return;
-                }
+        c.EmitDelegate<Action<ShopHelper, Player, NPC>>(
+            (shopHelper, player, npc) => {
+                TownGlobalNPC globalNPC = npc.GetGlobalNPC<TownGlobalNPC>();
 
-                // To prevent the "content" modifier from showing up when no other modifiers are present
-                shopHelper._currentHappiness = " ";
-
+                // Happiness bar will disappear if the string is empty for certain NPCs
+                shopHelper._currentHappiness = "A non-empty string";
                 globalNPC.MoodModule.ResetStaticModifiers();
-            }
-        );
 
-        c.GotoLastInstruction();
-        c.Emit(OpCodes.Ldarg_0);
-        c.Emit(OpCodes.Ldarg_2);
-        c.EmitDelegate<Action<ShopHelper, NPC>>(
-            (shopHelper, npc) => {
-                if (!npc.TryGetGlobalNPC(out TownGlobalNPC globalNPC)) {
-                    return;
+                List<NPC> npcNeighbors = shopHelper.GetNearbyResidentNPCs(npc, out int npcsWithinHouse, out int npcsWithinVillage);
+                bool[] npcNeighborsByType = new bool[NPCLoader.NPCCount];
+                foreach (NPC npcNeighbor in npcNeighbors) {
+                    npcNeighborsByType[npcNeighbor.type] = true;
                 }
 
-                TownNPCMoodModule moodModule = globalNPC.MoodModule;
+                HelperInfo info = new() { player = player, npc = npc, NearbyNPCs = npcNeighbors, nearbyNPCsByType = npcNeighborsByType };
+                if (shopHelper._database.TryGetProfileByNPCID(npc.type, out PersonalityProfile profile)) {
+                    foreach (IShopPersonalityTrait shopModifier in profile.ShopModifiers) {
+                        shopModifier.ModifyShopPrice(info, shopHelper);
+                    }
+                }
 
-                shopHelper._currentPriceAdjustment = MathHelper.Lerp(MinCostModifier, MaxCostModifier, 1f - moodModule.CurrentMood / TownNPCMoodModule.MaxMoodValue);
+                new AllPersonalitiesModifier().ModifyShopPrice(info, shopHelper);
+                shopHelper._currentPriceAdjustment = MathHelper.Lerp(MinCostModifier, MaxCostModifier, 1f - globalNPC.MoodModule.CurrentMood / TownNPCMoodModule.MaxMoodValue);
             }
         );
+        c.Emit(OpCodes.Ret);
+
+        // c.GotoLastInstruction();
+        // c.Emit(OpCodes.Ldarg_0);
+        // c.Emit(OpCodes.Ldarg_2);
+        // c.EmitDelegate<Action<ShopHelper, NPC>>(
+        //     (shopHelper, npc) => {
+        //         if (!npc.TryGetGlobalNPC(out TownGlobalNPC globalNPC)) {
+        //             return;
+        //         }
+        //
+        //         TownNPCMoodModule moodModule = globalNPC.MoodModule;
+        //
+        //         shopHelper._currentPriceAdjustment = MathHelper.Lerp(MinCostModifier, MaxCostModifier, 1f - moodModule.CurrentMood / TownNPCMoodModule.MaxMoodValue);
+        //     }
+        // );
     }
 
     // private static void HijackReportText(ILContext il) {
