dfcec64809a475088f2b3d7bdd1e0f6c9e39b343
MutantWafflez <61396445+mutantwafflez@users.noreply.github.com>
2025-02-06 13:11:37 -0800

    Decrease sleep deprivation random chance to sleep

--- Content/TownNPCRevitalization/Globals/NPCs/TownNPCModules/TownNPCSleepModule.cs
+++ Content/TownNPCRevitalization/Globals/NPCs/TownNPCModules/TownNPCSleepModule.cs
@@ -27,7 +27,7 @@ public sealed  class TownNPCSleepModule : TownNPCModule, IOnTownNPCAttack {
     private const float DefaultAwakeValue = MaxAwakeValue * 0.2f;
 
     private const int MaxBlockedSleepValue = LWMUtils.RealLifeSecond * 10;
-    private const int DefaultChanceToSleepWhileDeprived = 1000;
+    private const int DefaultChanceToSleepWhileDeprived = 5000;
 
     private static readonly SleepSchedule DefaultSleepSchedule = new(new TimeOnly(19, 30, 0), new TimeOnly(4, 30, 0));
     private static readonly Gradient<Color> SleepIconColorGradient = new (Color.Lerp, (0f, Color.Red), (0.5f, Color.DarkOrange), (1f, Color.White));
