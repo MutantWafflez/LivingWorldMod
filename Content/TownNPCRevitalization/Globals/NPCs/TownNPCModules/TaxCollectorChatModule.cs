9097589875a468fb2d55340c68354e90e5ce01c4
MutantWafflez <61396445+mutantwafflez@users.noreply.github.com>
2025-02-11 17:36:27 -0800

    Tax sheet beginnings

---
+++ Content/TownNPCRevitalization/Globals/NPCs/TownNPCModules/TaxCollectorChatModule.cs
@@ -0,0 +1,25 @@
+using LivingWorldMod.Content.TownNPCRevitalization.Globals.Hooks;
+using LivingWorldMod.Utilities;
+
+namespace LivingWorldMod.Content.TownNPCRevitalization.Globals.NPCs.TownNPCModules;
+
+/// <summary>
+///     Specific module for the <see cref="NPCID.TaxCollector" /> Town NPC that handles his specific chat interactions.
+/// </summary>
+public class TaxCollectorChatModule : TownNPCModule, ISetChatButtons {
+    public override bool AppliesToEntity(NPC entity, bool lateInstantiation) => lateInstantiation && entity.type == NPCID.TaxCollector;
+
+    public override void OnChatButtonClicked(NPC npc, bool firstButton) {
+        if (firstButton) {
+            return;
+        }
+
+        // Open UI here
+        Main.NewText("Click!");
+    }
+
+    // Add second button to tax collector chat box that opens a custom new UI
+    public void SetChatButtons(NPC npc, ref string buttonOne, ref string buttonTwo) {
+        buttonTwo = "NPCChatButtons.TaxCollector.TaxSheet".Localized().Value;
+    }
+}
