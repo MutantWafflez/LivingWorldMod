94a5fbc3c3b65d481a7d7bcc6c35b9a219a7a09b
MutantWafflez <61396445+mutantwafflez@users.noreply.github.com>
2025-03-30 16:41:25 -0700

    Fix waystones not syncing in multiplayer

--- Content/Waystones/Globals/PacketHandlers/WaystonePacketHandler.cs
+++ Content/Waystones/Globals/PacketHandlers/WaystonePacketHandler.cs
@@ -22,30 +22,34 @@ public class WaystonePacketHandler : PacketHandler {
 
         switch (packetType) {
             case InitiateWaystoneActivation:
-                if (Main.netMode == NetmodeID.Server) {
-                    int entityPosX = reader.ReadInt32();
-                    int entityPosY = reader.ReadInt32();
+                switch (Main.netMode) {
+                    case NetmodeID.Server: {
+                        int entityPosX = reader.ReadInt32();
+                        int entityPosY = reader.ReadInt32();
 
-                    if (!LWMUtils.TryFindModEntity(entityPosX, entityPosY, out WaystoneEntity entity) || entity.DoingActivationVFX) {
-                        return;
+                        if (!LWMUtils.TryFindModEntity(entityPosX, entityPosY, out WaystoneEntity entity) || entity.DoingActivationVFX) {
+                            return;
+                        }
+
+                        entity.ActivateWaystoneEntity();
+
+                        ModPacket packet = GetPacket();
+                        packet.Write(entityPosX);
+                        packet.Write(entityPosY);
+
+                        packet.Send(ignoreClient: fromWhomst);
+                        break;
                     }
+                    case NetmodeID.MultiplayerClient: {
+                        Point16 entityPos = new(reader.ReadInt32(), reader.ReadInt32());
 
-                    entity.ActivateWaystoneEntity();
+                        if (!LWMUtils.TryFindModEntity(entityPos.X, entityPos.Y, out WaystoneEntity entity)) {
+                            return;
+                        }
 
-                    ModPacket packet = GetPacket();
-                    packet.Write(entityPosX);
-                    packet.Write(entityPosY);
-
-                    packet.Send(ignoreClient: fromWhomst);
-                }
-                else if (Main.netMode == NetmodeID.MultiplayerClient) {
-                    Point16 entityPos = new(reader.ReadInt32(), reader.ReadInt32());
-
-                    if (!LWMUtils.TryFindModEntity(entityPos.X, entityPos.Y, out WaystoneEntity entity)) {
-                        return;
+                        WaystoneSystem.Instance.AddNewActivationEntity(entityPos.ToWorldCoordinates(16, 16), entity.WaystoneColor);
+                        break;
                     }
-
-                    WaystoneSystem.Instance.AddNewActivationEntity(entityPos.ToWorldCoordinates(16, 16), entity.WaystoneColor);
                 }
 
                 break;
--- Content/Waystones/Globals/Systems/WaystoneSystem.cs
+++ Content/Waystones/Globals/Systems/WaystoneSystem.cs
@@ -1,22 +1,47 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using LivingWorldMod.Content.Waystones.DataStructures.Classes;
+using LivingWorldMod.Content.Waystones.Tiles;
 using LivingWorldMod.Globals.BaseTypes.Systems;
+using LivingWorldMod.Utilities;
 using Microsoft.Xna.Framework;
 
 namespace LivingWorldMod.Content.Waystones.Globals.Systems;
 
 /// <summary>
-///     ModSystem that helps with client-side Waystone functionality.
+///     ModSystem that handles various miscellaneous waystone functionality. Namely, it handles updating <see cref="WaystoneActivationEntity" /> entities and syncing waystones across the server.
 /// </summary>
-[Autoload(Side = ModSide.Client)]
 public class WaystoneSystem : BaseModSystem<WaystoneSystem> {
     private List<WaystoneActivationEntity> _activationEntities;
 
     public override void OnWorldLoad() {
+        if (Main.netMode == NetmodeID.Server) {
+            return;
+        }
+
         _activationEntities = [];
     }
 
+    public override bool HijackGetData(ref byte messageType, ref BinaryReader reader, int playerNumber) {
+        // This message ID is sent as a part of the client joining process, where spawn tile information is usually synced.
+        // The name is slightly misleading however, as this is also where pylons are synced, so I feel it is only natural
+        // for waystones to also be synced properly here.
+        if (Main.netMode != NetmodeID.Server || messageType is not MessageID.SpawnTileData) {
+            return false;
+        }
+
+        foreach (WaystoneEntity entity in LWMUtils.GetAllEntityOfType<WaystoneEntity>()) {
+            NetMessage.SendData(MessageID.TileEntitySharing, playerNumber, number: entity.ID, number2: entity.Position.X, number3: entity.Position.Y);
+        }
+
+        return false;
+    }
+
     public override void PostUpdateEverything() {
+        if (Main.netMode == NetmodeID.Server) {
+            return;
+        }
+
         _activationEntities.RemoveAll(entity => entity.isFinished);
         _activationEntities.ForEach(entity => entity.Update());
     }
@@ -27,6 +52,10 @@ public class WaystoneSystem : BaseModSystem<WaystoneSystem> {
     /// <param name="entityCenter"> The center of the entity. </param>
     /// <param name="waystoneColor"> The color of the Waystone in question. </param>
     public void AddNewActivationEntity(Vector2 entityCenter, Color waystoneColor) {
+        if (Main.netMode == NetmodeID.Server) {
+            return;
+        }
+
         _activationEntities.Add(new WaystoneActivationEntity(entityCenter, waystoneColor));
     }
 }
