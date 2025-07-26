5b3d9cb4547a344c2a20057975cbd1704b210332
MutantWafflez <61396445+mutantwafflez@users.noreply.github.com>
2025-07-26 16:00:02 -0700

    Make UIHousingVillagerDisplay.cs a sub-class, small behavior changes

--- Content/Villages/UI/VillagerHousing/VillagerHousingUIState.cs
+++ Content/Villages/UI/VillagerHousing/VillagerHousingUIState.cs
@@ -1,10 +1,15 @@
-﻿using LivingWorldMod.Content.Villages.DataStructures.Enums;
+﻿using System.Runtime.InteropServices.Marshalling;
+using LivingWorldMod.Content.Villages.DataStructures.Enums;
 using LivingWorldMod.Content.Villages.Globals.BaseTypes.NPCs;
+using LivingWorldMod.DataStructures.Classes;
 using LivingWorldMod.Globals.UIElements;
 using LivingWorldMod.Utilities;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent;
 using Terraria.ModLoader.UI.Elements;
 using Terraria.UI;
 
@@ -14,6 +19,119 @@ namespace LivingWorldMod.Content.Villages.UI.VillagerHousing;
 ///     UIState that handles the visuals of the Housing UI.
 /// </summary>
 public class VillagerHousingUIState : UIState {
+    /// <summary>
+    ///     Element that shows a specific villager in the housing menu. An instance of this is created
+    ///     per villager that exists in the world of a given type when the player is in the housing menu.
+    /// </summary>
+    private class UIHousingVillagerDisplay : UIElement {
+        // Very arbitrary number, I know, but this is about the smallest we can get the
+        // width/height while keeping 3 objects in a row in the grid
+        private const float SideLength = 50.284f;
+
+        private const int LockIconSideLength = 22;
+
+        /// <summary>
+        ///     The villager instance that this element is displaying.
+        /// </summary>
+        public Villager myVillager;
+
+        /// <summary>
+        ///     Whether or not this villager is currently selected.
+        /// </summary>
+        public bool IsSelected => myVillager.NPC.whoAmI == Main.instance.mouseNPCIndex;
+
+        /// <summary>
+        ///     Whether or not this NPC is "allowed" to be housed, which is to say whether or not the
+        ///     village that the villager belongs to likes the player.
+        /// </summary>
+        //TODO: Swap back to commented expression when Reputation system is re-implemented
+        public bool IsAllowed => true; //myVillager.RelationshipStatus >= VillagerRelationship.Like;
+
+        public UIHousingVillagerDisplay(Villager villager) {
+            myVillager = villager;
+
+            Width = StyleDimension.FromPixels(SideLength);
+            Height = StyleDimension.FromPixels(SideLength);
+        }
+
+        public override void LeftClick(UIMouseEvent evt) {
+            //Prevent any interaction if the villagers do not like the player
+            if (!IsAllowed) {
+                return;
+            }
+
+            //Change the mouse type properly
+            if (IsSelected) {
+                Main.instance.SetMouseNPC(-1, -1);
+            }
+            else {
+                //Our IL edit in NPCHousingPatches.cs handles the drawing here.
+                Main.instance.SetMouseNPC(myVillager.NPC.whoAmI, myVillager.Type);
+                SoundEngine.PlaySound(SoundID.MenuTick);
+            }
+        }
+
+        public override void Update(GameTime gameTime) {
+            if (!ContainsPoint(Main.MouseScreen) || myVillager is null) {
+                return;
+            }
+
+            Main.LocalPlayer.mouseInterface = true;
+            Main.instance.MouseText(IsAllowed ? myVillager.NPC.GivenName : "UI.VillagerHousing.VillagerTypeLocked".Localized().FormatWith(myVillager.VillagerType.ToString()));
+        }
+
+        protected override void DrawChildren(SpriteBatch spriteBatch) {
+            base.DrawChildren(spriteBatch);
+
+            spriteBatch.Draw(
+                TextureAssets.InventoryBack11.Value,
+                GetDimensions().ToRectangle(),
+                null,
+                !IsAllowed ? Color.Gray : Color.White,
+                0f,
+                default(Vector2),
+                SpriteEffects.None,
+                0f
+            );
+
+
+            LayeredDrawObject drawObject = myVillager.drawObject;
+            Rectangle textureDrawRegion = new(0, 0, drawObject.GetLayerFrameWidth(), drawObject.GetLayerFrameHeight(0, 0, Main.npcFrameCount[myVillager.Type]));
+
+            drawObject.Draw(
+                spriteBatch,
+                new DrawData(
+                    null,
+                    GetDimensions().Center(),
+                    textureDrawRegion,
+                    IsSelected ? Color.Yellow : Color.White,
+                    0f,
+                    new Vector2(textureDrawRegion.Width / 2f, textureDrawRegion.Height / 2f * 1.15f),
+                    0.75f,
+                    SpriteEffects.None
+                ),
+                myVillager.DrawIndices
+            );
+
+            if (IsAllowed) {
+                return;
+            }
+
+            //Draw lock icon if not "allowed" (player is not high enough rep)
+            spriteBatch.Draw(
+                TextureAssets.HbLock[0].Value,
+                GetDimensions().Center(),
+                new Rectangle(0, 0, LockIconSideLength, LockIconSideLength),
+                Color.White,
+                0f,
+                new Vector2(LockIconSideLength / 2f, LockIconSideLength / 2f),
+                1.25f,
+                SpriteEffects.None,
+                0f
+            );
+        }
+    }
+
     /// <summary>
     ///     The Villager type to currently be showing to the player.
     /// </summary>
@@ -55,11 +173,6 @@ public class VillagerHousingUIState : UIState {
     ///     The grid of villagers of the specified type that are currently being displayed.
     /// </summary>
     public UIGrid gridOfVillagers;
-
-    /// <summary>
-    ///     The displacement of the menu icon based on the map and its current mode/placement.
-    /// </summary>
-    private int _mapDisplacement;
 
     /// <summary>
     ///     Path to the sprites for this UI.
@@ -116,34 +229,21 @@ public class VillagerHousingUIState : UIState {
         villagerHousingZone.Append(gridOfVillagers);
     }
 
-    protected override void DrawChildren(SpriteBatch spriteBatch) {
+    public override void Update(GameTime gameTime) {
         bool isMiniMapEnabled = !Main.mapFullscreen && Main.mapStyle == 1;
-
-        //Adapted vanilla code since "Main.mH" is private, and I do not want to use reflection every frame
-        _mapDisplacement = 0;
-
-        if (Main.mapEnabled) {
-            if (isMiniMapEnabled) {
-                _mapDisplacement = 256;
-            }
-
-            if (_mapDisplacement + Main.instance.RecommendedEquipmentAreaPushUp > Main.screenHeight) {
-                _mapDisplacement = Main.screenHeight - Main.instance.RecommendedEquipmentAreaPushUp;
-            }
-        }
 
         //Update positions
         openMenuButton.Left.Set(Main.screenWidth - (isMiniMapEnabled ? 220f : 177f), 0f);
-        openMenuButton.Top.Set((isMiniMapEnabled ? 143f : 114f) + _mapDisplacement, 0f);
+        openMenuButton.Top.Set((isMiniMapEnabled ? 143f : 114f) + Main.mH, 0f);
 
-        villagerHousingZone.Top.Set(180f + _mapDisplacement, 0f);
+        villagerHousingZone.Top.Set(180f + Main.mH, 0f);
 
         //Disable Menu Visibility when any other equip page buttons are pressed
         if (villagerHousingZone.IsVisible && Main.EquipPageSelected != -1) {
             CloseMenu();
         }
-
-        base.DrawChildren(spriteBatch);
+        
+        base.Update(gameTime);
     }
 
     /// <summary>
@@ -202,20 +302,20 @@ public class VillagerHousingUIState : UIState {
         //Clear list for re-displaying
         gridOfVillagers.Clear();
 
-        for (int i = 0; i < Main.maxNPCs; i++) {
-            if (Main.npc[i].active && Main.npc[i].ModNPC is Villager villager && villager.VillagerType == typeToShow) {
-                UIHousingVillagerDisplay element = new(villager);
-
-                element.Activate();
-
-                gridOfVillagers.Add(element);
+        foreach (NPC npc in Main.ActiveNPCs) {
+            if (npc.ModNPC is not Villager villager || villager.VillagerType != typeToShow) {
+                continue;
             }
+
+            UIHousingVillagerDisplay element = new(villager);
+
+            gridOfVillagers.Add(element);
         }
 
-        gridScrollbar.Activate();
+        gridScrollbar.ViewPosition = 0f;
     }
 
-    private void WhileHoveringButton() {
+    private static void WhileHoveringButton() {
         Main.instance.MouseText("UI.VillagerHousing.ButtonHoverText".Localized().Value);
     }
 }
