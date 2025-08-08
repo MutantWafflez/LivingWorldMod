ec1e1db17c65c320df2447146b433cfc549db9c4
MutantWafflez <61396445+mutantwafflez@users.noreply.github.com>
2025-08-07 21:41:13 -0700

    Create the will-be AI state for dog fetching

---
+++ Content/TownNPCRevitalization/AIStates/DogFetchAIState.cs
@@ -0,0 +1,9 @@
+using LivingWorldMod.Content.TownNPCRevitalization.Globals.ModTypes;
+
+namespace LivingWorldMod.Content.TownNPCRevitalization.AIStates;
+
+public class DogFetchAIState : TownNPCAIState {
+    public override void DoState(NPC npc) {
+        
+    }
+}
