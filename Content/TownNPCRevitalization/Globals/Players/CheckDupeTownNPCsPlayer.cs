809e5a43c64207016d6fecf5705db658e97f3d6d
MutantWafflez <61396445+mutantwafflez@users.noreply.github.com>
2025-12-21 18:35:07 -0800

    Add kill dupe command

---
+++ Content/TownNPCRevitalization/Globals/Players/CheckDupeTownNPCsPlayer.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using LivingWorldMod.Content.TownNPCRevitalization.Globals.NPCs;
+using LivingWorldMod.Utilities;
+
+namespace LivingWorldMod.Content.TownNPCRevitalization.Globals.Players;
+
+// TODO: Fix actual underlying issue necessitating this Player class 
+public class CheckDupeTownNPCsPlayer : ModPlayer {
+    public static Dictionary<int, List<int>> GetAllTownNPCs() {
+        Dictionary<int, List<int>> allTownNPCs = [];
+        foreach (NPC npc in Main.ActiveNPCs) {
+            if (!TownGlobalNPC.EntityIsValidTownNPC(npc, true)) {
+                continue;
+            }
+
+            if (!allTownNPCs.TryGetValue(npc.type, out List<int> whoAmIs)) {
+                allTownNPCs[npc.type] = [npc.whoAmI];
+
+                continue;
+            }
+
+            whoAmIs.Add(npc.whoAmI);
+        }
+
+        return allTownNPCs;
+    }
+
+    public override void OnEnterWorld() {
+        Dictionary<int, List<int>> allTownNPCs = GetAllTownNPCs();
+
+        if (!allTownNPCs.Any(pair => pair.Value.Count > 1)) {
+            return;
+        }
+
+        Main.NewText("Debug.DuplicateNPCsWarning".Localized(), LWMUtils.YellowErrorTextColor);
+    }
+}
