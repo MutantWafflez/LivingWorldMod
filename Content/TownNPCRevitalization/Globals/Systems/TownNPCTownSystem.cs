715e10209c65986d0bf91ca0d533e260f69c3a98
MutantWafflez <61396445+mutantwafflez@users.noreply.github.com>
2026-02-14 15:56:18 -0800

    Start of Town System

---
+++ Content/TownNPCRevitalization/Globals/Systems/TownNPCTownSystem.cs
@@ -0,0 +1,11 @@
+using LivingWorldMod.Globals.BaseTypes.Systems;
+
+namespace LivingWorldMod.Content.TownNPCRevitalization.Globals.Systems;
+
+/// <summary>
+///     System that handles the conglomeration of Town NPCs in the same area, internally referred to as a "Town". Manages the shared usage of a pathfinder grid for performance, "Points of Interest" for
+///     selecting places for Town NPCs to pathfind to, and more to-be implemented.
+/// </summary>
+public class TownNPCTownSystem : BaseModSystem<TownNPCTownSystem> {
+    
+}
