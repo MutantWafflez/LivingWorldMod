e1ffdc54ab741f8aa3c64f153dfd79c3d23a8f38
MutantWafflez <61396445+mutantwafflez@users.noreply.github.com>
2026-04-12 01:55:52 -0700

    Use proper casting methods (Unsafe is VERY un-necessary)

--- DataStructures/Records/HashPoint.cs
+++ DataStructures/Records/HashPoint.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Diagnostics;
 using System.Numerics;
-using System.Runtime.CompilerServices;
 using Microsoft.Xna.Framework;
 using Vector2 = Microsoft.Xna.Framework.Vector2;
 
@@ -16,11 +15,11 @@ namespace LivingWorldMod.DataStructures.Records;
 public record struct HashPoint<T>(T X, T Y) where T : struct, INumber<T> {
     public static HashPoint<T> NegativeOne {
         get;
-    } = new (Unsafe.BitCast<int, T>(-1));
+    } = new (T.CreateTruncating(-1));
 
     public static HashPoint<T> Zero {
         get;
-    } = new (Unsafe.BitCast<int, T>(0));
+    } = new (T.CreateTruncating(0));
 
     private string DebugDisplayString => $"{X.ToString()} {Y.ToString()}";
 
@@ -34,11 +33,11 @@ public record struct HashPoint<T>(T X, T Y) where T : struct, INumber<T> {
 
     public static HashPoint<T> operator /(HashPoint<T> point1, HashPoint<T> point2) => new (point1.X / point2.X, point1.Y / point2.Y);
 
-    public static explicit operator Vector2(HashPoint<T> hashPoint) => new (Unsafe.BitCast<T, float>(hashPoint.X), Unsafe.BitCast<T, float>(hashPoint.Y));
+    public static explicit operator Vector2(HashPoint<T> hashPoint) => new (float.CreateTruncating(hashPoint.X), float.CreateTruncating(hashPoint.Y));
 
-    public static implicit operator Point(HashPoint<T> hashPoint) => new (Unsafe.BitCast<T, int>(hashPoint.X), Unsafe.BitCast<T, int>(hashPoint.Y));
+    public static implicit operator Point(HashPoint<T> hashPoint) => new (int.CreateTruncating(hashPoint.X), int.CreateTruncating(hashPoint.Y));
 
-    public static implicit operator HashPoint<T>(Point point) => new (Unsafe.BitCast<int, T>(point.X), Unsafe.BitCast<int, T>(point.Y));
+    public static implicit operator HashPoint<T>(Point point) => new (T.CreateTruncating(point.X), T.CreateTruncating(point.Y));
 
     public override int GetHashCode() => X.GetHashCode() + Y.GetHashCode();
 
@@ -47,7 +46,7 @@ public record struct HashPoint<T>(T X, T Y) where T : struct, INumber<T> {
     /// <summary>
     ///     Returns the distance between this point and another point in the form of a double, regardless of the type associated with this point.
     /// </summary>
-    public double Distance(HashPoint<T> other) => Math.Sqrt(Unsafe.BitCast<T, double>(DistanceSquared(other)));
+    public double Distance(HashPoint<T> other) => Math.Sqrt(double.CreateTruncating(DistanceSquared(other)));
 
     /// <summary>
     ///     Returns the un-sqrt'd distance between this point and another, i.e. the distance squared.
