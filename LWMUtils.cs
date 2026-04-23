04f0bbf2b01f30299bbe2bc63f3ae4e389fe5113
MutantWafflez <61396445+mutantwafflez@users.noreply.github.com>
2026-04-22 23:27:02 -0700

    EXTENSION MEMBERS WOOO BABYYY

--- LWMUtils.cs
+++ LWMUtils.cs
@@ -221,22 +221,24 @@ public static partial class LWMUtils {
 
     private static Exception ILInstructionNotFoundException => new ("Could not find specified IL instruction.");
 
-    /// <summary>
-    ///     Extension for strings that will turn the provided string into its <see cref="LocalizedText" /> equivalent, assuming
-    ///     the provided string is the key for a <see cref="LWM" /> localization file text value.
-    /// </summary>
-    public static LocalizedText Localized(this string key) => LWM.Instance.GetLocalization(key);
+    extension(string key) {
+        /// <summary>
+        ///     Extension for strings that will turn the provided string into its <see cref="LocalizedText" /> equivalent, assuming
+        ///     the provided string is the key for a <see cref="LWM" /> localization file text value.
+        /// </summary>
+        public LocalizedText Localized() => LWM.Instance.GetLocalization(key);
+
+        /// <summary>
+        ///     Extension for strings that will prepend "Mods.LivingWorldMod." to the string, for use with generating localization keys
+        ///     instead of <see cref="LocalizedText" /> objects directly.
+        /// </summary>
+        public string PrependModKey() => $"Mods.{nameof(LivingWorldMod)}.{key}";
+    }
 
     /// <summary>
     ///     Converts this <see cref="LocalizedText" /> instance into its <see cref="NetworkText" /> equivalent.
     /// </summary>
     public static NetworkText ToNetworkText(this LocalizedText text) => NetworkText.FromKey(text.Key);
-
-    /// <summary>
-    ///     Extension for strings that will prepend "Mods.LivingWorldMod." to the string, for use with generating localization keys
-    ///     instead of <see cref="LocalizedText" /> objects directly.
-    /// </summary>
-    public static string PrependModKey(this string suffix) => $"Mods.{nameof(LivingWorldMod)}.{suffix}";
 
     /// <summary>
     ///     Takes the input string and places a space in-between each english capital letter, returning the result.
